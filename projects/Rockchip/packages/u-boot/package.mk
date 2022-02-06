29213f76993bd837bfbd708ddd60633ff995a8d2
fewtarius <fewtarius@jelos.org>
2022-02-06 13:17:28 -0500

    Revert uboot for the 351, will revisit later.

--- projects/Rockchip/packages/u-boot/package.mk
+++ projects/Rockchip/packages/u-boot/package.mk
@@ -13,6 +13,12 @@ PKG_VERSION="543c8d3"
 GET_HANDLER_SUPPORT="git"
 PKG_GIT_CLONE_DEPTH="1"
 
+if [[ "${DEVICE}" =~ RG351 ]]
+then
+  PKG_VERSION="2b575e133fa1280dcf07f0bc5ebc4e6a48ea7132"
+  PKG_URL="https://github.com/351ELEC/uboot_rg351.git"
+fi
+
 PKG_IS_KERNEL_PKG="yes"
 PKG_STAMP="$UBOOT_SYSTEM"
 
