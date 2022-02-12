9cec3eb33b982bea53b84bfabc80f9a43fa21fac
fewtarius <fewtarius@jelos.org>
2022-02-11 20:03:17 -0500

    * Bump kernel * Add max fps patch, thanks to @shantigilbert

--- projects/Rockchip/packages/linux/package.mk
+++ projects/Rockchip/packages/linux/package.mk
@@ -5,7 +5,7 @@
 
 PKG_NAME="linux"
 PKG_URL="https://github.com/JustEnoughLinuxOS/rockchip-kernel.git"
-PKG_VERSION="1cdebf47e6d6a9e8638ec10641b775d7f99b241a"
+PKG_VERSION="913d87135"
 PKG_GIT_CLONE_BRANCH="main"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kernel.org"
