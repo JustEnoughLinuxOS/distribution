cf1779e0f49220cd9e1706430770bab7e26966ad
fewtarius <fewtarius@jelos.org>
2022-02-24 21:10:03 -0500

    Update rkbin URL since upstream is gone.

--- packages/tools/rkbin/package.mk
+++ packages/tools/rkbin/package.mk
@@ -2,10 +2,10 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="rkbin"
-PKG_VERSION="7d631e0d5b2d373b54d4533580d08fb9bd2eaad4"
+PKG_VERSION="7fd0297"
 PKG_ARCH="arm aarch64"
 PKG_LICENSE="nonfree"
-PKG_SITE="https://github.com/rockchip-linux/rkbin"
-PKG_URL="https://github.com/rockchip-linux/rkbin/archive/$PKG_VERSION.tar.gz"
+PKG_SITE="https://github.com/JustEnoughLinuxOS/rkbin"
+PKG_URL="${PKG_SITE}.git"
 PKG_LONGDESC="rkbin: Rockchip Firmware and Tool Binaries"
 PKG_TOOLCHAIN="manual"
