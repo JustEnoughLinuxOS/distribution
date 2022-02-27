b4222da6a3bf5620e3dec2f8efb9b8389238c05e
fewtarius <fewtarius@jelos.org>
2022-02-26 19:43:54 -0500

    Update Art Book theme.

--- packages/themes/es-theme-art-book-next/package.mk
+++ packages/themes/es-theme-art-book-next/package.mk
@@ -1,20 +1,20 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2020 Shanti Gilbert (https://github.com/shantigilbert)
 # Copyright (C) 2020 351ELEC team (https://github.com/fewtarius/351ELEC)
+# Copyright (C) 2021 Fewtarius
 
 PKG_NAME="es-theme-art-book-next"
-PKG_REV="1"
-PKG_VERSION="eb75225"
+PKG_VERSION="5ca7b7e63f5151f00a208fe0283154ccf477e734"
 PKG_ARCH="any"
 PKG_LICENSE="CUSTOM"
 PKG_SITE="https://github.com/anthonycaccese/es-theme-art-book-next"
-PKG_URL="$PKG_SITE.git"
+PKG_URL="${PKG_SITE}.git"
 GET_HANDLER_SUPPORT="git"
 PKG_SHORTDESC="ArtBook"
 PKG_LONGDESC="Art Book theme"
 PKG_TOOLCHAIN="manual"
 
 makeinstall_target() {
-  mkdir -p $INSTALL/usr/config/emulationstation/themes/$PKG_NAME
-  cp -rf * $INSTALL/usr/config/emulationstation/themes/$PKG_NAME
+  mkdir -p ${INSTALL}/usr/config/emulationstation/themes/${PKG_NAME}
+  cp -rf * ${INSTALL}/usr/config/emulationstation/themes/${PKG_NAME}
 }
