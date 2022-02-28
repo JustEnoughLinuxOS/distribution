ec6ee4d71284363b1e6667f8e02f479a65f645b5
fewtarius <fewtarius@jelos.org>
2022-02-28 18:04:41 -0500

    Update librga to use JELOS repo.

--- packages/devel/librga/package.mk
+++ packages/devel/librga/package.mk
@@ -2,11 +2,11 @@
 # Copyright (C) 2020-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="librga"
-PKG_VERSION="1fc02d56d97041c86f01bc1284b7971c6098c5fb"
+PKG_VERSION="df26244"
 PKG_ARCH="arm aarch64"
 PKG_LICENSE="GNU"
 PKG_DEPENDS_TARGET="toolchain libdrm"
-PKG_SITE="https://github.com/rockchip-linux/linux-rga"
-PKG_URL="$PKG_SITE/archive/$PKG_VERSION.tar.gz"
+PKG_SITE="https://github.com/JustEnoughLinuxOS/linux-rga"
+PKG_URL="${PKG_SITE}.git"
 PKG_LONGDESC="The RGA driver userspace "
 PKG_TOOLCHAIN="auto"
