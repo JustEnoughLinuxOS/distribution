be3d0be78d89b6da9fbb3fb6a6b73acac9b6e464
fewtarius <fewtarius@jelos.org>
2022-03-02 18:16:51 -0500

    Add support for the RTL8821CU adapter.

---
+++ packages/kernel/linux-drivers/RTL8821CU/package.mk
@@ -0,0 +1,31 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+# Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
+# Copyright (C) 2021-present Fewtarius
+
+PKG_NAME="RTL8821CU"
+PKG_VERSION="ef3ff12"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/brektrou/rtl8821CU"
+PKG_URL="${PKG_SITE}.git"
+PKG_DEPENDS_TARGET="toolchain linux"
+PKG_NEED_UNPACK="${LINUX_DEPENDS}"
+PKG_LONGDESC="Realtek 8821CU driver for 4.4-5.x"
+PKG_IS_KERNEL_PKG="yes"
+
+pre_make_target() {
+  unset LDFLAGS
+}
+
+make_target() {
+  make V=1 \
+       ARCH=${TARGET_KERNEL_ARCH} \
+       KSRC=$(kernel_path) \
+       CROSS_COMPILE=${TARGET_KERNEL_PREFIX} \
+       CONFIG_POWER_SAVING=n
+}
+
+makeinstall_target() {
+  mkdir -p ${INSTALL}/$(get_full_module_dir)/${PKG_NAME}
+    cp *.ko ${INSTALL}/$(get_full_module_dir)/${PKG_NAME}
+}
