7e05d244db08fd8a21d35015ac5e5e10922d8d1e
fewtarius <fewtarius@jelos.org>
2022-03-06 18:44:42 -0500

    Merged RAM and GPU OC clock to main.

--- projects/Rockchip/packages/linux/package.mk
+++ projects/Rockchip/packages/linux/package.mk
@@ -6,7 +6,7 @@
 PKG_NAME="linux"
 PKG_URL="https://github.com/JustEnoughLinuxOS/rockchip-kernel.git"
 PKG_VERSION="263f128a1"
-PKG_GIT_CLONE_BRANCH="oc"
+PKG_GIT_CLONE_BRANCH="main"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kernel.org"
 PKG_DEPENDS_HOST="ccache:host openssl:host"
