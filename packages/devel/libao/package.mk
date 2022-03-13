00a3eede183de96bd43a1f08db1f7a5d95b32957
fewtarius <fewtarius@jelos.org>
2022-03-13 17:45:57 -0400

    Add a few packages for future use.

---
+++ packages/devel/libao/package.mk
@@ -0,0 +1,11 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2022-present Fewtarius
+
+PKG_NAME="libao"
+PKG_VERSION="cafce902"
+PKG_LICENSE="GPL"
+PKG_SITE="https://gitlab.xiph.org/xiph/libao"
+PKG_URL="${PKG_SITE}.git"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_LONGDESC="Audio output portability library."
+PKG_TOOLCHAIN="autotools"
