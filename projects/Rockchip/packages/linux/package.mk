18afd3933ed34b3903eee3921157fc44a952ac5f
fewtarius <fewtarius@jelos.org>
2022-03-13 07:39:45 -0400

    Bump due to a bit of cleanup.

--- projects/Rockchip/packages/linux/package.mk
+++ projects/Rockchip/packages/linux/package.mk
@@ -5,7 +5,7 @@
 
 PKG_NAME="linux"
 PKG_URL="https://github.com/JustEnoughLinuxOS/rockchip-kernel.git"
-PKG_VERSION="fafaf3016"
+PKG_VERSION="d523a185e"
 PKG_GIT_CLONE_BRANCH="main"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kernel.org"
