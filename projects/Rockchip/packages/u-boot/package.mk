28c7b33fce503eb8a61b58467fd2f423ccae9b7b
fewtarius <fewtarius@jelos.org>
2022-03-26 06:22:01 -0400

    Need an older rkbin and firmware for uboot.

--- projects/Rockchip/packages/u-boot/package.mk
+++ projects/Rockchip/packages/u-boot/package.mk
@@ -13,18 +13,18 @@ PKG_VERSION="23f4a5d"
 GET_HANDLER_SUPPORT="git"
 
 PKG_IS_KERNEL_PKG="yes"
-PKG_STAMP="$UBOOT_SYSTEM"
+PKG_STAMP="${UBOOT_SYSTEM}"
 
-[ -n "$ATF_PLATFORM" ] && PKG_DEPENDS_TARGET+=" atf"
+[ -n "${ATF_PLATFORM}" ] && PKG_DEPENDS_TARGET+=" atf"
 
-PKG_NEED_UNPACK="$PROJECT_DIR/$PROJECT/bootloader"
-[ -n "$DEVICE" ] && PKG_NEED_UNPACK+=" $PROJECT_DIR/$PROJECT/devices/$DEVICE/bootloader"
+PKG_NEED_UNPACK="${PROJECT}_DIR/${PROJECT}/bootloader"
+[ -n "${DEVICE}" ] && PKG_NEED_UNPACK+=" ${PROJECT}_DIR/${PROJECT}/devices/${DEVICE}/bootloader"
 
 PKG_PATCH_DIRS="patches/${DEVICE}"
 
 post_patch() {
-  if [ -n "$UBOOT_SYSTEM" ] && find_file_path bootloader/config; then
-    PKG_CONFIG_FILE="$PKG_BUILD/configs/$($ROOT/$SCRIPTS/uboot_helper $PROJECT $DEVICE $UBOOT_SYSTEM config)"
+  if [ -n "${UBOOT_SYSTEM}" ] && find_file_path bootloader/config; then
+    PKG_CONFIG_FILE="${PKG_BUILD}/configs/$(${ROOT}/${SCRIPTS}/uboot_helper ${PROJECT} ${DEVICE} ${UBOOT_SYSTEM} config)"
     if [ -f "$PKG_CONFIG_FILE" ]; then
       cat $FOUND_PATH >> "$PKG_CONFIG_FILE"
     fi
@@ -32,32 +32,32 @@ post_patch() {
 }
 
 make_target() {
-  if [ -z "$UBOOT_SYSTEM" ]; then
+  if [ -z "${UBOOT_SYSTEM}" ]; then
     echo "UBOOT_SYSTEM must be set to build an image"
     echo "see './scripts/uboot_helper' for more information"
   else
     [ "${BUILD_WITH_DEBUG}" = "yes" ] && PKG_DEBUG=1 || PKG_DEBUG=0
     [ -n "${ATF_PLATFORM}" ] &&  cp -av $(get_build_dir atf)/bl31.bin .
-    DEBUG=${PKG_DEBUG} CROSS_COMPILE="$TARGET_KERNEL_PREFIX" LDFLAGS="" ARCH=arm make mrproper
-    DEBUG=${PKG_DEBUG} CROSS_COMPILE="$TARGET_KERNEL_PREFIX" LDFLAGS="" ARCH=arm make $($ROOT/$SCRIPTS/uboot_helper $PROJECT $DEVICE $UBOOT_SYSTEM config)
-    DEBUG=${PKG_DEBUG} CROSS_COMPILE="$TARGET_KERNEL_PREFIX" LDFLAGS="" ARCH=arm _python_sysroot="$TOOLCHAIN" _python_prefix=/ _python_exec_prefix=/ make HOSTCC="$HOST_CC" HOSTLDFLAGS="-L$TOOLCHAIN/lib" HOSTSTRIP="true" CONFIG_MKIMAGE_DTC_PATH="scripts/dtc/dtc"
+    DEBUG=${PKG_DEBUG} CROSS_COMPILE="${TARGET_KERNEL_PREFIX}" LDFLAGS="" ARCH=arm make mrproper
+    DEBUG=${PKG_DEBUG} CROSS_COMPILE="${TARGET_KERNEL_PREFIX}" LDFLAGS="" ARCH=arm make $(${ROOT}/${SCRIPTS}/uboot_helper ${PROJECT} ${DEVICE} ${UBOOT_SYSTEM} config)
+    DEBUG=${PKG_DEBUG} CROSS_COMPILE="${TARGET_KERNEL_PREFIX}" LDFLAGS="" ARCH=arm _python_sysroot="${TOOLCHAIN}" _python_prefix=/ _python_exec_prefix=/ make HOSTCC="$HOST_CC" HOSTLDFLAGS="-L${TOOLCHAIN}/lib" HOSTSTRIP="true" CONFIG_MKIMAGE_DTC_PATH="scripts/dtc/dtc"
   fi
 }
 
 makeinstall_target() {
-    mkdir -p $INSTALL/usr/share/bootloader
+    mkdir -p ${INSTALL}/usr/share/bootloader
     # Only install u-boot.img et al when building a board specific image
     if [ -n "${UBOOT_SYSTEM}" ]; then
       find_file_path bootloader/install && . ${FOUND_PATH}
     fi
 
     # Always install the update script
-    find_file_path bootloader/update.sh && cp -av ${FOUND_PATH} $INSTALL/usr/share/bootloader
+    find_file_path bootloader/update.sh && cp -av ${FOUND_PATH} ${INSTALL}/usr/share/bootloader
 
     # Always install the canupdate script
     if find_file_path bootloader/canupdate.sh; then
-      cp -av ${FOUND_PATH} $INSTALL/usr/share/bootloader
-      sed -e "s/@PROJECT@/${DEVICE:-$PROJECT}/g" \
-          -i $INSTALL/usr/share/bootloader/canupdate.sh
+      cp -av ${FOUND_PATH} ${INSTALL}/usr/share/bootloader
+      sed -e "s/@PROJECT@/${DEVICE:-${PROJECT}}/g" \
+          -i ${INSTALL}/usr/share/bootloader/canupdate.sh
     fi
 }
