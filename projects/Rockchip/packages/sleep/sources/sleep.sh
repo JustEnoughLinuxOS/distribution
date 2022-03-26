ef86c2327d38765fae4ddc8164796627047a02dc
fewtarius <fewtarius@jelos.org>
2022-03-26 12:15:38 -0400

    DWC2 doesn't sleep properly on the RG351x.

--- projects/Rockchip/packages/sleep/sources/sleep.sh
+++ projects/Rockchip/packages/sleep/sources/sleep.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
+. /etc/profile
 
 case $1 in
-   pre)
+  pre)
     # Store system brightness
     cat /sys/class/backlight/backlight/brightness > /storage/.brightness
     # Store sound state. Try to avoid having max volume after resume
     alsactl store -f /tmp/asound.state
     systemctl stop headphones
+
+    # RG351x devices are notorious for losing USB when they sleep.
+    if [[ "${HW_DEVICE}" =~ RG351 ]]
+    then
+      modprobe -r dwc2
+    fi
+
+    # stop hotkey service
+    systemctl stop odroidgoa-headphones.service
  
     # This file is used by ES to determine if we just woke up from sleep
     touch /run/.last_sleep_time
 
-    ;;
-   post)
+  ;;
+  post)
     # Restore pre-sleep sound state
     alsactl restore -f /tmp/asound.state
     VOL=$(get_setting "audio.volume" 2>/dev/null)
@@ -22,5 +32,11 @@ case $1 in
     cat /storage/.brightness > /sys/class/backlight/backlight/brightness
     # re-detect and reapply sound, brightness and hp state
     systemctl start headphones
-	;;
+
+    if [[ "${HW_DEVICE}" =~ RG351 ]]
+    then
+      modprobe -i dwc2
+    fi
+
+  ;;
 esac
