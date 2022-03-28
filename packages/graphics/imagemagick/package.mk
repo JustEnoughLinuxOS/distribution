397622d14182abaa84aadcb00346bafde4dd765e
fewtarius <fewtarius@jelos.org>
2022-03-28 16:06:00 -0400

    Improving compatibility with the 351 series.

--- packages/graphics/imagemagick/package.mk
+++ packages/graphics/imagemagick/package.mk
@@ -1,12 +1,12 @@
 # SPDX-License-Identifier: GPL-2.0
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
+# Copyright (C) 2022-present Fewtarius
 
 PKG_NAME="imagemagick"
-PKG_VERSION="7.0.10-62"
-#PKG_SHA256=""
+PKG_VERSION="4c0b7d2"
 PKG_LICENSE="http://www.imagemagick.org/script/license.php"
-PKG_SITE="http://www.imagemagick.org/"
-PKG_URL="https://github.com/ImageMagick/ImageMagick/archive/$PKG_VERSION.tar.gz"
+PKG_SITE="https://github.com/ImageMagick/ImageMagick"
+PKG_URL="${PKG_SITE}.git"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="Software suite to create, edit, compose, or convert bitmap images"
 
@@ -18,5 +18,6 @@ PKG_CONFIGURE_OPTS_TARGET="--disable-openmp \
                            --with-x=no"
 
 makeinstall_target() {
-  make install DESTDIR=$INSTALL $PKG_MAKEINSTALL_OPTS_TARGET
+  make install DESTDIR=${INSTALL} ${PKG_MAKEINSTALL_OPTS_TARGET}
+  rm ${INSTALL}/usr/bin/*config
 }
