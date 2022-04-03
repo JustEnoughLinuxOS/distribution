e49d672ce2965f81916e9d3ecc8cf791512492d5
fewtarius <fewtarius@jelos.org>
2022-04-03 14:09:21 -0400

    Add .sh extensions for ES.

--- packages/sysutils/rclone/package.mk
+++ packages/sysutils/rclone/package.mk
@@ -27,6 +27,6 @@ makeinstall_target() {
   cp ${PKG_DIR}/sources/rsync.conf ${INSTALL}/usr/config/
   chmod 755 ${INSTALL}/usr/bin/rclone
   mkdir -p ${INSTALL}/usr/config/modules
-  ln -sf /usr/bin/cloud_backup ${INSTALL}/usr/config/modules/cloud_backup
-  ln -sf /usr/bin/cloud_restore ${INSTALL}/usr/config/modules/cloud_restore
+  ln -sf /usr/bin/cloud_backup ${INSTALL}/usr/config/modules/cloud_backup.sh
+  ln -sf /usr/bin/cloud_restore ${INSTALL}/usr/config/modules/cloud_restore.sh
 }
