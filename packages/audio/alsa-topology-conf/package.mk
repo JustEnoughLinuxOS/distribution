5e70b789c43739c397ffeb0c4ebb89cb4dcde488
fewtarius <fewtarius@jelos.org>
2022-04-14 06:24:44 -0400

    Update some of our system packages from CoreELEC upstream.

---
+++ packages/audio/alsa-topology-conf/package.mk
@@ -0,0 +1,16 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2020-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="alsa-topology-conf"
+PKG_VERSION="1.2.5.1"
+PKG_SHA256="f7c5bae1545abcd73824bc97f4e72c340e11abea188ba0f1c06f5e0ad776b179"
+PKG_LICENSE="BSD-3c"
+PKG_SITE="http://www.alsa-project.org/"
+PKG_URL="ftp://ftp.alsa-project.org/pub/lib/alsa-topology-conf-${PKG_VERSION}.tar.bz2"
+PKG_LONGDESC="ALSA topology configuration files"
+PKG_TOOLCHAIN="manual"
+
+makeinstall_target() {
+  mkdir -p ${INSTALL}/usr/share/alsa/
+  cp -PR ${PKG_BUILD}/topology ${INSTALL}/usr/share/alsa/
+}
