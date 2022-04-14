37b787dc2e952cff52f64fe5543ec1f97b41d47b
fewtarius <fewtarius@jelos.org>
2022-04-14 07:56:57 -0400

    Work on base os updates.

--- packages/network/connman/package.mk
+++ packages/network/connman/package.mk
@@ -3,7 +3,7 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="connman"
-PKG_VERSION="25479cf866e60b0101cda0d80e825cd8128bb4eb" # 1.40
+PKG_VERSION="7a0bc35c4b2d9bd1c6fd76866a8a86807a626591" # 1.41
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.connman.net"
 PKG_URL="https://git.kernel.org/pub/scm/network/connman/connman.git/snapshot/connman-$PKG_VERSION.tar.gz"
