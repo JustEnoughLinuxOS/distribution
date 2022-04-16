4dc3db2e621000b3d93c2761ce857651b71c3d22
Brooksytech <macebrooks@gmail.com>
2022-04-16 05:53:05 -0700

    Install MelonDS emulator (#18)
    * Adding MelonDS
    
    * melonds adding to Jelos package.mk
    
    * Update es_systems.cfg
    
    * update drastic location
    
    * fix typo
    
    * Update package.mk
    
    * Update package.mk
    
    * Update package.mk
    
    * Update package.mk
    
    * Update package.mk
    
    * Update package.mk
    
    * Update package.mk
    
    * Update package.mk
    
    * Update package.mk
    
    * Update package.mk
    
    * Update package.mk
    
    * Update package.mk
    
    * Update package.mk
    
    * Update package.mk
    
    * Update package.mk
    
    * Update package.mk
    
    * Update package.mk
    
    * Update package.mk
    
    * Update package.mk
    
    * adding back in melon DS
    
    * fix typo
    
    * Update es_features.cfg
    
    * Update retroarch-core-options.cfg

---
+++ packages/games/libretro/melonds/package.mk
@@ -0,0 +1,34 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2022-present BrooksyTech (https://github.com/brooksytech)
+
+PKG_NAME="melonds"
+PKG_VERSION="e93ec3e462d3dfc1556781510a3cee113f02abb2"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="GPLv3"
+PKG_SITE="https://git.libretro.com/libretro/melonDS"
+PKG_URL="$PKG_SITE.git"
+PKG_DEPENDS_TARGET="toolchain $OPENGLES"
+PKG_PRIORITY="optional"
+PKG_SECTION="libretro"
+PKG_SHORTDESC="MeloDS - Nintendo DS emulator for libretro"
+PKG_TOOLCHAIN="make"
+
+pre_make_target() {
+
+  cd ${PKG_BUILD}
+  rm CMakeLists.txt
+  
+  if [[ "${DEVICE}" =~ RG351 ]]
+  then
+    PKG_MAKE_OPTS_TARGET=" platform=odroidgoa"
+  elif [[ "${DEVICE}" =~ RG552 ]]
+  then
+    PKG_MAKE_OPTS_TARGET=" platform=RK3399"
+  fi
+}
+
+makeinstall_target() {
+  mkdir -p $INSTALL/usr/lib/libretro
+  cp melonds_libretro.so $INSTALL/usr/lib/libretro/
+}
