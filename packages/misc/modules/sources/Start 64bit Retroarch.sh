dd14d63980c93fd7e9e6c0d36757e8139117d032
fewtarius <fewtarius@jelos.org>
2022-04-17 11:19:10 -0400

    Add 32bit FLAC for RetroArch.

--- packages/misc/modules/sources/Start 64bit Retroarch.sh
+++ packages/misc/modules/sources/Start 64bit Retroarch.sh
@@ -3,10 +3,8 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2020-present Fewtarius
 
-source /usr/bin/env.sh
 source /etc/profile
 
 jslisten set "killall retroarch"
 
 /usr/bin/retroarch --appendconfig /usr/config/retroarch/retroarch64bit-append.cfg
-/usr/bin/clear-retroarch.sh
