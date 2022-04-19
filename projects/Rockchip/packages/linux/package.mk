cfccc6e922a252fba1a78f583557c3ff28e20376
fewtarius <fewtarius@jelos.org>
2022-04-19 18:01:26 -0400

    * Eliminate the top overclocks as we're not using them anyway. * Apply the default clocks in init to reduce risk of instability during boot.y

--- projects/Rockchip/packages/linux/package.mk
+++ projects/Rockchip/packages/linux/package.mk
@@ -6,7 +6,7 @@
 
 PKG_NAME="linux"
 PKG_URL="https://github.com/JustEnoughLinuxOS/rockchip-kernel.git"
-PKG_VERSION="b259f0437"
+PKG_VERSION="1a8d74081"
 PKG_GIT_CLONE_BRANCH="main"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kernel.org"
