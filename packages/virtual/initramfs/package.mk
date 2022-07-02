7f33d74c82601eafe68b434bad109e217a6acfe5
fewtarius <fewtarius@jelos.org>
2022-07-02 16:56:42 -0400

    Fixes.

--- packages/virtual/initramfs/package.mk
+++ packages/virtual/initramfs/package.mk
@@ -1,42 +1,38 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
-# Copyright (C) 2020-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="initramfs"
 PKG_VERSION=""
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.openelec.tv"
 PKG_URL=""
-PKG_DEPENDS_INIT="libc:init busybox:init util-linux:init e2fsprogs:init dosfstools:init terminus-font:init bkeymaps:init"
-PKG_DEPENDS_TARGET="toolchain initramfs:init"
+PKG_DEPENDS_TARGET="toolchain libc:init busybox:init linux:init plymouth-lite:init util-linux:init e2fsprogs:init dosfstools:init exfat:init fakeroot:host terminus-font:init"
 PKG_SECTION="virtual"
-PKG_LONGDESC="Metapackage for installing initramfs"
+PKG_LONGDESC="debug is a Metapackage for installing initramfs"
 
-if [ "${ISCSI_SUPPORT}" = yes ]; then
-  PKG_DEPENDS_INIT+=" open-iscsi:init"
+if [ "$ISCSI_SUPPORT" = yes ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET open-iscsi:init"
 fi
 
-if [ "${INITRAMFS_PARTED_SUPPORT}" = yes ]; then
-  PKG_DEPENDS_INIT+=" parted:init"
+if [ "$INITRAMFS_PARTED_SUPPORT" = yes ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET parted:init"
 fi
-
-for i in ${PKG_DEPENDS_INIT}; do
-  PKG_NEED_UNPACK+=" $(get_pkg_directory ${i})"
-done
 
 post_install() {
-  if [ "$BUILD_ANDROID_BOOTIMG" = "yes" ]; then
-  ( 
-    cd $BUILD/initramfs
+  ( cd $BUILD/initramfs
+    if [ "$TARGET_ARCH" = "x86_64" ]; then
+      ln -sfn /usr/lib $BUILD/initramfs/lib64
+      mkdir -p $BUILD/initramfs/usr
+      ln -sfn /usr/lib $BUILD/initramfs/usr/lib64
+    fi
 
-    ln -sfn /usr/lib  $BUILD/initramfs/lib
-    ln -sfn /usr/bin  $BUILD/initramfs/bin
+    ln -sfn /usr/lib $BUILD/initramfs/lib
+    ln -sfn /usr/bin $BUILD/initramfs/bin
     ln -sfn /usr/sbin $BUILD/initramfs/sbin
 
-    mkdir -p $BUILD/image
+    mkdir -p $BUILD/image/
     fakeroot -- sh -c \
       "mkdir -p dev; mknod -m 600 dev/console c 5 1; find . | cpio -H newc -ov -R 0:0 > $BUILD/image/initramfs.cpio"
   )
-  fi
-} 
+}
