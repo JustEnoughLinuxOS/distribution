cd88c52167fce310ab068c0664981afa67cfcbb6
brooksytech <macebrooks@gmail.com>
2022-07-16 08:31:50 -0700

    changes

--- projects/Rockchip/packages/u-boot/package.mk
+++ projects/Rockchip/packages/u-boot/package.mk
@@ -14,11 +14,11 @@ PKG_VERSION="23f4a5d"
 GET_HANDLER_SUPPORT="git"
 PKG_PATCH_DIRS+="${DEVICE}"
 
-if [[ "${DEVICE}" =~ RG552 ]]
-then
-  PKG_URL="https://github.com/R-ARM/u-boot.git"
-  PKG_VERSION="b9bb224329a5ca77c8f2708c85bea19d971b610e"
-fi
+#if [[ "${DEVICE}" =~ RG552 ]]
+#then
+#  PKG_URL="https://github.com/R-ARM/u-boot.git"
+#  PKG_VERSION="b9bb224329a5ca77c8f2708c85bea19d971b610e"
+#fi
 
 if [[ "${DEVICE}" =~ RG351 ]]
 then
