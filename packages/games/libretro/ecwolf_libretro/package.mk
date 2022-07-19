180c8bb44774483e55f54e024de37c643737c4b1
brooksytech <macebrooks@gmail.com>
2022-07-19 07:42:30 -0700

    ecwolf libretro add missing dependencies

--- packages/games/libretro/ecwolf_libretro/package.mk
+++ packages/games/libretro/ecwolf_libretro/package.mk
@@ -6,7 +6,7 @@ PKG_VERSION="1c82ef94f25a358774d4b9daa44537ab406a1203"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/ecwolf"
 PKG_URL="${PKG_SITE}.git"
-PKG_DEPENDS_TARGET="toolchain"
+PKG_DEPENDS_TARGET="toolchain SDL2 SDL2_mixer SDL2_net libjpeg-turbo bzip2"
 PKG_SHORTDESC="ECWolf is a port of the Wolfenstein 3D engine based of Wolf4SDL."
 PKG_TOOLCHAIN="make"
 
