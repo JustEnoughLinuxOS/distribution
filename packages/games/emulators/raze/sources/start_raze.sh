06c627f8a0cd1af0ed120073a03a7262461bcbd1
travis134 <travis134@gmail.com>
2022-08-18 04:00:13 -0700

    Should always run raze bin?

--- packages/games/emulators/raze/sources/start_raze.sh
+++ packages/games/emulators/raze/sources/start_raze.sh
@@ -11,13 +11,11 @@ RUN_DIR="/storage/roms/build"
 CONFIG="/storage/.config/game/raze/raze.ini"
 SAVE_DIR="/storage/roms/gamedata/raze"
 
-if [ ! -L "/storage/.config/raze" ]
-then
+if [ ! -L "/storage/.config/raze" ]; then
   ln -sf "/storage/.config/game/raze" "/storage/.config/raze"
 fi
 
-if [ ! -f "/storage/.config/game/raze/raze.ini" ]
-then
+if [ ! -f "/storage/.config/game/raze/raze.ini" ]; then
   cp -rf /usr/config/game/raze/raze.ini /storage/.config/game/raze/
 fi
 
@@ -37,10 +35,11 @@ if [ ${EXT} == "build" ]; then
     if [ "$key" == "GRP" ]; then
       params+=" -gamegrp $value"
     fi
-    done < "${1}"
+  done <"${1}"
 fi
 
 if [[ ! "$RUN_DIR" == "/storage/roms/build" ]]; then
   cd "${RUN_DIR}"
-  /usr/bin/raze ${params} >/var/log/raze.log 2>&1
 fi
+
+/usr/bin/raze ${params} >/var/log/raze.log 2>&1
