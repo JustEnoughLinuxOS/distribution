d69faa60bd5a780decee66bf8daa7691ae8fac03
travis134 <travis134@gmail.com>
2022-08-18 18:42:47 -0700

    Thinking about it more, if device-specific overrides exist we want them to stomp common

--- packages/games/emulators/raze/package.mk
+++ packages/games/emulators/raze/package.mk
@@ -50,11 +50,11 @@ makeinstall_target() {
   chmod 0755 ${INSTALL}/usr/bin/*
 
   mkdir -p ${INSTALL}/usr/config/game/raze
-  cp -rf ${PKG_DIR}/config/games ${INSTALL}/usr/config/game/raze
+  cp ${PKG_DIR}/config/games ${INSTALL}/usr/config/game/raze
   chmod 0755 ${INSTALL}/usr/config/game/raze/games/*sh
   if [ -d "${PKG_DIR}/config/${DEVICE}" ]
   then
-    cp ${PKG_DIR}/config/${DEVICE}/* ${INSTALL}/usr/config/game/raze
+    cp -rf ${PKG_DIR}/config/${DEVICE}/* ${INSTALL}/usr/config/game/raze
   fi
   cp ${PKG_BUILD}/.${TARGET_NAME}/*.pk3 ${INSTALL}/usr/config/game/raze
   cp -r ${PKG_BUILD}/.${TARGET_NAME}/soundfonts ${INSTALL}/usr/config/game/raze
