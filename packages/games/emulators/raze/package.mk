01152ae4c3de9a46521a640a37c84e7494057d31
travis134 <travis134@gmail.com>
2022-08-20 07:42:40 -0700

    Consolidate Raze default settings across devices

--- packages/games/emulators/raze/package.mk
+++ packages/games/emulators/raze/package.mk
@@ -50,11 +50,13 @@ makeinstall_target() {
   chmod 0755 ${INSTALL}/usr/bin/*
 
   mkdir -p ${INSTALL}/usr/config/game/raze
-  cp -rf ${PKG_DIR}/config/games ${INSTALL}/usr/config/game/raze
+  cp -rf ${PKG_DIR}/config/common/* ${INSTALL}/usr/config/game/raze
   chmod 0755 ${INSTALL}/usr/config/game/raze/games/*sh
   if [ -d "${PKG_DIR}/config/${DEVICE}" ]
   then
     cp -rf ${PKG_DIR}/config/${DEVICE}/* ${INSTALL}/usr/config/game/raze
+    cat ${INSTALL}/usr/config/game/raze/defaults.ini >> ${INSTALL}/usr/config/game/raze/raze.ini
+    rm cat ${INSTALL}/usr/config/game/raze/defaults.ini
   fi
   cp ${PKG_BUILD}/.${TARGET_NAME}/*.pk3 ${INSTALL}/usr/config/game/raze
   cp -r ${PKG_BUILD}/.${TARGET_NAME}/soundfonts ${INSTALL}/usr/config/game/raze
