189439347835f273f0ba54139c5ce0d770b60e6c
fewtarius <fewtarius@jelos.org>
2022-08-23 07:23:08 -0400

    Drop hard coded splash screens and generate a correct resolution/aspect splash during the build.

--- packages/ui/splash/package.mk
+++ packages/ui/splash/package.mk
@@ -7,20 +7,36 @@ PKG_ARCH="any"
 PKG_LICENSE="apache2"
 PKG_SITE=""
 PKG_URL=""
+PKG_DEPENDS_TARGET="toolchain imagemagick"
 PKG_LONGDESC="Boot splash screens"
 PKG_TOOLCHAIN="manual"
 
+make_init() {
+  :
+}
+
 make_target() {
- :
+  :
+}
+
+mksplash() {
+  SPLASH="${ROOT}/distributions/JELOS/splash/splash.png"
+  if [ -n "${DISPLAY_RESOLUTION}" ]
+  then
+    convert ${SPLASH} -background none -resize ${DISPLAY_RESOLUTION} -gravity center -extent ${DISPLAY_RESOLUTION} ${1}/splash.png
+    convert ${SPLASH} -rotate 270 -background none -resize ${DISPLAY_RESOLUTION} -gravity center -extent ${DISPLAY_RESOLUTION} ${1}/splashl.png
+  else
+    cp ${SPLASH} ${INSTALL}/splash
+    convert ${SPLASH} -rotate 270 -background none ${INSTALL}/splash/splashl.png
+  fi
+}
+
+makeinstall_init() {
+  mkdir -p ${INSTALL}/splash
+  mksplash ${INSTALL}/splash
 }
 
 makeinstall_target() {
-  mkdir -p ${INSTALL}/splash
-  find_file_path splash/splash.conf && cp ${FOUND_PATH} ${INSTALL}/splash
-  find_file_path "splash/splash-*.png" && cp ${FOUND_PATH} ${INSTALL}/splash
-}
-
-post_makeinstall_target() {
   mkdir -p ${INSTALL}/usr/config/splash
-  find_file_path "splash/splash-*.png" && cp ${FOUND_PATH} ${INSTALL}/usr/config/splash
+  mksplash ${INSTALL}/usr/config/splash
 }
