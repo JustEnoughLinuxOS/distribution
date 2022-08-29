9ba7af775182d19b3ff86b05749c30b2e6c1e6b6
fewtarius <fewtarius@jelos.org>
2022-08-28 20:04:17 -0400

    Move base_only guard to misc-packages.  May move to image later.

--- packages/virtual/misc-packages/package.mk
+++ packages/virtual/misc-packages/package.mk
@@ -1,15 +1,20 @@
 # SPDX-License-Identifier: GPL-2.0
 # Copyright (C) 2018 Team LibreELEC (https://libreelec.tv)
 # Copyright (C) 2018-present Team CoreELEC (https://coreelec.org)
+# Copyright (C) 2022-present Fewtarius
 
 PKG_NAME="misc-packages"
 PKG_VERSION=""
 PKG_LICENSE="GPL"
-PKG_SITE="https://libreelec.tv"
+PKG_SITE="https://www.jelos.org"
 PKG_URL=""
-PKG_DEPENDS_TARGET="toolchain Python3 jq ${ADDITIONAL_PACKAGES}"
 PKG_SECTION="virtual"
 PKG_LONGDESC="misc-packages: Metapackage for miscellaneous packages"
+
+if [ ! "${BASE_ONLY}" = "true" ]
+then
+  PKG_DEPENDS_TARGET+=" ${ADDITIONAL_PACKAGES}"
+fi
 
 # Entware support
 if [ "$ENTWARE_SUPPORT" = "yes" ]; then
