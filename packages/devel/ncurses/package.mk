711677cf6b3c560d10729de789e0dd8de86ac43a
fewtarius <fewtarius@jelos.org>
2022-08-30 06:33:48 -0400

    ncurses should be fixed, thanks in part to CoreELEC.

--- packages/devel/ncurses/package.mk
+++ packages/devel/ncurses/package.mk
@@ -3,10 +3,10 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="ncurses"
-PKG_VERSION="6.3-20220827"
+PKG_VERSION="6.3"
 PKG_LICENSE="MIT"
 PKG_SITE="http://www.gnu.org/software/ncurses/"
-PKG_URL="http://invisible-mirror.net/archives/ncurses/current/ncurses-${PKG_VERSION}.tgz"
+PKG_URL="http://invisible-mirror.net/archives/ncurses/ncurses-${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_HOST="ccache:host"
 PKG_DEPENDS_TARGET="toolchain zlib ncurses:host"
 PKG_LONGDESC="A library is a free software emulation of curses in System V Release 4.0, and more."
@@ -47,7 +47,7 @@ PKG_CONFIGURE_OPTS_TARGET="
                            --disable-sigwinch \
                            --enable-pc-files \
                            --with-pkg-config-libdir=/usr/lib/pkgconfig \
-                           --disable-tcap-names \
+                           --enable-tcap-names \
                            --without-develop \
                            --disable-hard-tabs \
                            --disable-xmc-glitch \
@@ -57,7 +57,7 @@ PKG_CONFIGURE_OPTS_TARGET="
                            --enable-widec \
                            --disable-echo \
                            --disable-warnings \
-                           --disable-home-terminfo \
+                           --enable-home-terminfo \
                            --enable-lib-suffixes \
                            --disable-assertions"
 
@@ -69,7 +69,17 @@ PKG_CONFIGURE_OPTS_HOST="--enable-termcap \
                          --without-manpages"
 
 post_makeinstall_target() {
+  local f
   cp misc/ncurses-config ${TOOLCHAIN}/bin
   chmod +x ${TOOLCHAIN}/bin/ncurses-config
-  sed -e "s:\(['=\" ]\)/usr:\\1${SYSROOT_PREFIX}/usr:g" -i ${TOOLCHAIN}/bin/ncurses-config
+  sed -e "s:\(['=\" ]\)/usr:\\1${PKG_ORIG_SYSROOT_PREFIX}/usr:g" -i ${TOOLCHAIN}/bin/ncurses-config
+  rm -f ${TOOLCHAIN}/bin/ncurses6-config
+  rm -rf ${INSTALL}/usr/bin
+  # create links to be compatible with any ncurses include path and lib names
+  ln -sf . ${SYSROOT_PREFIX}/usr/include/ncursesw
+  ln -sf . ${SYSROOT_PREFIX}/usr/include/ncurses
+  for f in form menu ncurses panel; do
+    ln -sf lib${f}w.a ${SYSROOT_PREFIX}/usr/lib/lib${f}.a
+    ln -sf ${f}w.pc ${SYSROOT_PREFIX}/usr/lib/pkgconfig/${f}.pc
+  done
 }
