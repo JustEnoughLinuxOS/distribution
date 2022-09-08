6c0e0f68de085aca42392b3d05413805329a4ba2
fewtarius <fewtarius@jelos.org>
2022-09-08 19:10:37 -0400

    Fall back to the 3.0 branch of wxwidgets.

--- packages/graphics/wxwidgets/package.mk
+++ packages/graphics/wxwidgets/package.mk
@@ -2,15 +2,14 @@
 # Copyright (C) 2020-present Fewtarius
 
 PKG_NAME="wxwidgets"
-PKG_VERSION="7ad1bffa875f7a38db58b6069ea3ff0c49c211d2"
+PKG_VERSION="87ad6fa"
 PKG_LICENSE="wxWindows Library Licence"
 PKG_SITE="https://github.com/wxWidgets/wxWidgets"
 PKG_URL="${PKG_SITE}.git"
 PKG_DEPENDS_TARGET="toolchain zlib libpng libjpeg-turbo gdk-pixbuf gtk3"
 PKG_LONGDESC="wxWidgets is a free and open source cross-platform C++ framework for writing advanced GUI applications using native controls."
 PKG_BUILD_FLAGS="+pic"
-
-PKG_CMAKE_OPTS_TARGET+=" -DwxUSE_GUI=no"
+PKG_GIT_CLONE_BRANCH="WX_3_0_BRANCH"
 
 pre_configure_target() {
   LDFLAGS="${LDFLAGS} -fPIC"
