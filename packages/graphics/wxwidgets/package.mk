bae378a30e1b9a570629fb660aea3610aeae006e
fewtarius <fewtarius@jelos.org>
2022-09-11 17:10:06 -0400

    Work on pcsx2 and pcsx2 libretro.

--- packages/graphics/wxwidgets/package.mk
+++ packages/graphics/wxwidgets/package.mk
@@ -6,7 +6,7 @@ PKG_VERSION="7ad1bffa875f7a38db58b6069ea3ff0c49c211d2"
 PKG_LICENSE="wxWindows Library Licence"
 PKG_SITE="https://github.com/wxWidgets/wxWidgets"
 PKG_URL="${PKG_SITE}.git"
-PKG_DEPENDS_TARGET="toolchain zlib libpng libjpeg-turbo gdk-pixbuf gtk3"
+PKG_DEPENDS_TARGET="toolchain zlib libpng libjpeg-turbo gdk-pixbuf gtk3 libaio"
 PKG_LONGDESC="wxWidgets is a free and open source cross-platform C++ framework for writing advanced GUI applications using native controls."
 PKG_BUILD_FLAGS="+pic"
 
