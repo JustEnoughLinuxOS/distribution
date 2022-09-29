7a4b04aad52ba770f87993aa4683dc1513e52097
brooksytech <macebrooks@gmail.com>
2022-09-28 20:58:19 -0700

    Add BSNES core & Citra libretro 3DS

---
+++ packages/games/libretro/bsnes/package.mk
@@ -0,0 +1,32 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2022-present BrooksyTech (https://github.com/brooksytech)
+
+PKG_NAME="bsnes"
+PKG_VERSION="b04ea7236fc05c2e2c5dc645bbdba8c90196bb5f"
+PKG_LICENSE="GPLv2"
+PKG_SITE="https://github.com/libretro/bsnes-libretro"
+PKG_URL="$PKG_SITE/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_SECTION="libretro"
+PKG_SHORTDESC="BSNES Super Nintendo Libretro Core"
+PKG_IS_ADDON="no"
+PKG_TOOLCHAIN="make"
+PKG_AUTORECONF="no"
+
+if [ ! "${OPENGL}" = "no" ]; then
+  PKG_DEPENDS_TARGET+=" ${OPENGL} glu libglvnd"
+fi
+
+if [ "${OPENGLES_SUPPORT}" = yes ]; then
+  PKG_DEPENDS_TARGET+=" ${OPENGLES}"
+fi
+
+#make_target() {
+ # make PROFILE=performance
+#}
+
+makeinstall_target() {
+  mkdir -p $INSTALL/usr/lib/libretro
+  cp bsnes_libretro.so $INSTALL/usr/lib/libretro/
+}
+
