3c8bd21b42b7b9ed8040c03a601806688f66373d
fewtarius <fewtarius@jelos.org>
2022-10-25 07:30:07 -0400

    Fix missing symbol issue with readline (Fixes vice, etc).

--- packages/devel/readline/package.mk
+++ packages/devel/readline/package.mk
@@ -17,6 +17,11 @@ PKG_CONFIGURE_OPTS_TARGET="bash_cv_wcwidth_broken=no \
                            --disable-static \
                            --with-curses"
 
+pre_configure_target() {
+  export LDFLAGS="${LDFLAGS} -lncursesw -ltinfow"
+  export CFLAGS="${CFLAGS} -fcommon"
+}
+
 post_makeinstall_target() {
   sed -i 's/-lreadline/-lreadline -lncursesw/' ${SYSROOT_PREFIX}/usr/lib/pkgconfig/readline.pc
   rm -rf ${INSTALL}/usr/share/readline
