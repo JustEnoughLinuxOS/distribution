d467eecd2293f0bd725673e1ec1d85b79ae26408
fewtarius <fewtarius@jelos.org>
2022-10-27 17:28:59 -0400

    Part one of the PSP libretro path fix.

--- packages/games/libretro/ppsspp/package.mk
+++ packages/games/libretro/ppsspp/package.mk
@@ -28,33 +28,67 @@ PKG_LONGDESC="A PSP emulator for Android, Windows, Mac, Linux and Blackberry 10,
 GET_HANDLER_SUPPORT="git"
 
 PKG_LIBNAME="ppsspp_libretro.so"
-PKG_LIBPATH="lib/$PKG_LIBNAME"
+PKG_LIBPATH="lib/${PKG_LIBNAME}"
 
-pre_configure_target() {
-  PKG_CMAKE_OPTS_TARGET="-DLIBRETRO=ON \
-                         -DUSE_SYSTEM_FFMPEG=ON \
-                         -DUSING_X11_VULKAN=OFF"
 
-  if [ "${ARCH}" = "arm" ] && [ ! "${TARGET_CPU}" = "arm1176jzf-s" ]; then
-    PKG_CMAKE_OPTS_TARGET+=" -DARMV7=ON"
-  elif [ "${TARGET_CPU}" = "arm1176jzf-s" ]; then
-    PKG_CMAKE_OPTS_TARGET+=" -DARM=ON"
-  fi
+if [ ! "${OPENGL}" = "no" ]; then
+  PKG_DEPENDS_TARGET+=" ${OPENGL} glu libglvnd glew"
+  PKG_CMAKE_OPTS_TARGET+=" -DUSING_FBDEV=OFF \
+                           -DUSING_GLES2=OFF"
+fi
 
-  if [ "${OPENGLES_SUPPORT}" = "yes" ]; then
-    PKG_CMAKE_OPTS_TARGET+=" -DUSING_FBDEV=ON \
-                             -DUSING_EGL=ON \
-                             -DUSING_GLES2=ON"
-  fi
-}
+if [ "${OPENGLES_SUPPORT}" = yes ]; then
+  PKG_DEPENDS_TARGET+=" ${OPENGLES}"
+  PKG_CMAKE_OPTS_TARGET+=" -DUSING_FBDEV=ON \
+                           -DUSING_EGL=OFF \
+                           -DUSING_GLES2=ON \
+                           -DVULKAN=OFF \
+                           -DUSE_VULKAN_DISPLAY_KHR=OFF\
+                           -DUSING_X11_VULKAN=OFF"
+fi
+
+### Vulkan is still not working for PPSSPP on the win600 yet.
+#if [ "${VULKAN_SUPPORT}" = "yes" ]
+#then
+#  PKG_DEPENDS_TARGET+=" vulkan-loader vulkan-headers"
+#  PKG_CMAKE_OPTS_TARGET+=" -DUSE_VULKAN_DISPLAY_KHR=ON \
+#                           -DVULKAN=ON \
+#                           -DEGL_NO_X11=1
+#                           -DMESA_EGL_NO_X11_HEADERS=1"
+#else
+#  PKG_CMAKE_OPTS_TARGET+=" -DVULKAN=OFF"
+#fi
+
+if [ "${DISPLAYSERVER}" = "wl" ]; then
+  PKG_DEPENDS_TARGET+=" wayland ${WINDOWMANAGER}"
+  PKG_CMAKE_OPTS_TARGET+=" -DUSE_WAYLAND_WSI=ON"
+else
+  PKG_CMAKE_OPTS_TARGET+=" -DUSE_WAYLAND_WSI=OFF"
+fi
+
+PKG_CMAKE_OPTS_TARGET+="${PKG_CMAKE_OPTS_TARGET} \
+                        -DUSE_SYSTEM_FFMPEG=OFF \
+                        -DCMAKE_BUILD_TYPE=Release \
+                        -DCMAKE_SYSTEM_NAME=Linux \
+                        -DBUILD_SHARED_LIBS=OFF \
+                        -DANDROID=OFF \
+                        -DWIN32=OFF \
+                        -DAPPLE=OFF \
+			-DLIBRETRO=ON \
+                        -DCMAKE_CROSSCOMPILING=ON \
+                        -DUSING_QT_UI=OFF \
+                        -DUNITTEST=OFF \
+                        -DSIMULATOR=OFF \
+                        -DHEADLESS=OFF \
+                        -DUSE_DISCORD=OFF"
 
 pre_make_target() {
   # fix cross compiling
-  find $PKG_BUILD -name flags.make -exec sed -i "s:isystem :I:g" \{} \;
-  find $PKG_BUILD -name build.ninja -exec sed -i "s:isystem :I:g" \{} \;
+  find ${PKG_BUILD} -name flags.make -exec sed -i "s:isystem :I:g" \{} \;
+  find ${PKG_BUILD} -name build.ninja -exec sed -i "s:isystem :I:g" \{} \;
 }
 
 makeinstall_target() {
-  mkdir -p $INSTALL/usr/lib/libretro
-  cp $PKG_LIBPATH $INSTALL/usr/lib/libretro/
+  mkdir -p ${INSTALL}/usr/lib/libretro
+  cp ${PKG_LIBPATH} ${INSTALL}/usr/lib/libretro/
 }
