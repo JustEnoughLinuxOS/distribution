485975426dde29c3f8bb2113d9cebf2d54266232
fewtarius <fewtarius@jelos.org>
2022-12-25 19:05:34 +0000

    * Pull over p8-platform, thanks to @CoreELEC. * Fix up moonlight dependencies.

--- packages/games/native/moonlight/package.mk
+++ packages/games/native/moonlight/package.mk
@@ -7,7 +7,7 @@ PKG_ARCH="any"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/moonlight-stream/moonlight-embedded"
 PKG_URL="$PKG_SITE.git"
-PKG_DEPENDS_TARGET="toolchain opus SDL2 libevdev alsa curl enet avahi"
+PKG_DEPENDS_TARGET="toolchain opus SDL2 libevdev alsa curl enet avahi libvdpau libcec ffmpeg"
 PKG_SHORTDESC="Moonlight Embedded is an open source implementation of NVIDIA's GameStream, as used by the NVIDIA Shield, but built for Linux."
 PKG_TOOLCHAIN="cmake"
 GET_HANDLER_SUPPORT="git"
@@ -19,7 +19,7 @@ then
 fi
 
 if [ ! "${OPENGL}" = "no" ]; then
-  PKG_DEPENDS_TARGET+=" ${OPENGL} glu"
+  PKG_DEPENDS_TARGET+=" ${OPENGL} glu libglvnd"
 fi
 
 if [ "${OPENGLES_SUPPORT}" = yes ]; then
