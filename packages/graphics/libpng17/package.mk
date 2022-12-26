e8f3ec8e025937de32086518559c463283c091a4
fewtarius <fewtarius@jelos.org>
2022-12-26 06:45:36 -0500

    Update libpng to fix a conflict.

--- packages/graphics/libpng17/package.mk
+++ packages/graphics/libpng17/package.mk
@@ -1,12 +1,13 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+# Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libpng17"
 PKG_VERSION="1.7.0beta89"
 PKG_SHA256="1fad2475a24174f5b4ad237b8b899a2c0583237f108c2288a6e2ac5c3537147a"
 PKG_LICENSE="LibPNG2"
 PKG_SITE="http://www.libpng.org/"
-PKG_URL="$SOURCEFORGE_SRC/libpng/libpng-$PKG_VERSION.tar.xz"
+PKG_URL="${SOURCEFORGE_SRC}/libpng/${PKG_NAME}-${PKG_VERSION}.tar.xz"
 PKG_DEPENDS_HOST="zlib:host"
 PKG_DEPENDS_TARGET="toolchain zlib"
 PKG_LONGDESC="An extensible file format for the lossless, portable, well-compressed storage of raster images."
@@ -14,23 +15,16 @@ PKG_TOOLCHAIN="configure"
 PKG_BUILD_FLAGS="+pic +pic:host"
 
 PKG_CONFIGURE_OPTS_TARGET="ac_cv_lib_z_zlibVersion=yes \
-                           --enable-shared \
-                           --disable-static"
+                           --disable-static \
+                           --enable-shared"
 
-PKG_CONFIGURE_OPTS_HOST="--enable-shared --disable-static"
+PKG_CONFIGURE_OPTS_HOST="--disable-static --enable-shared"
 
 pre_configure_host() {
-  export CPPFLAGS="$CPPFLAGS -I$TOOLCHAIN/include"
+  export CPPFLAGS="${CPPFLAGS} -I${TOOLCHAIN}/include"
 }
 
 pre_configure_target() {
-  export CPPFLAGS="$CPPFLAGS -I$SYSROOT_PREFIX/usr/include"
+  export CPPFLAGS="${CPPFLAGS} -I${SYSROOT_PREFIX}/usr/include"
 }
 
-post_makeinstall_target() {
-  sed -e "s:\([\"'= ]\)/usr:\\1$SYSROOT_PREFIX/usr:g" \
-      -e "s:libs=\"-lpng17\":libs=\"-lpng17 -lz\":g" \
-      -i $SYSROOT_PREFIX/usr/bin/libpng*-config
-
- rm -rf $INSTALL/usr/lib/libpng.so
-}
