b82d71351f7fda2300cc5ff72305120022abacd1
fewtarius <fewtarius@jelos.org>
2022-12-30 00:55:04 +0000

    Fix building yabasanshiro.

--- packages/games/libretro/yabasanshiro/package.mk
+++ packages/games/libretro/yabasanshiro/package.mk
@@ -46,6 +46,9 @@ fi
 
 pre_configure_target() {
   case ${DEVICE} in
+    RG353P|RG503|RG552|RGB20S)
+      PKG_MAKE_OPTS_TARGET+=" -C yabause/src/libretro platform=rockpro64 HAVE_NEON=0 FORCE_GLES=1"
+    ;;
     *)
       PKG_MAKE_OPTS_TARGET+=" -C yabause/src/libretro"
     ;;
