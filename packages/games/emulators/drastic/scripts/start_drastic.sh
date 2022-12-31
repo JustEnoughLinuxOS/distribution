6b41ee24775582d41c5c62fa4ca12b49177f7486
Brooksytech <macebrooks@gmail.com>
2022-12-31 14:45:50 -0800

    Update start_drastic.sh

--- packages/games/emulators/drastic/scripts/start_drastic.sh
+++ packages/games/emulators/drastic/scripts/start_drastic.sh
@@ -5,40 +5,11 @@
 . /etc/profile
 
 if [ ! -d "/storage/.config/drastic/aarch64/drastic" ]; then
-
-  echo -e "=> ${OS_NAME} DRASTIC INSTALLATION"
-  echo -e "\nChecking for internet connection..."
-
-  INETUP=$(/usr/bin/amionline >/dev/null 2>&1)
-  if [ $? == 0 ]
-  then
     mkdir -p "/storage/.config/drastic/aarch64"
     cd /storage/.config/drastic/aarch64
-
-    echo -e "\nFetching Drastic..."
-
-    curl -Lo drastic.tar.gz https://github.com/brooksytech/JelosAddOns/raw/main/drastic.tar.gz
-
-    echo -e "\nExtracting...\n"
+    cp /usr/config/drastic/drastic.tar.gz drastic.tar.gz
     tar -xvf drastic.tar.gz
-
     rm drastic.tar.gz
-
-    if [ ! -d "/storage/.config/drastic/aarch64/drastic/config" ]; then
-      mkdir -p /storage/.config/drastic/aarch64/drastic/config
-      cp /usr/config/drastic/drastic.cfg /storage/.config/drastic/aarch64/drastic/config/drastic.cfg
-    fi
-
-    clear
-
-  else
-    echo -e "Please connect to the internet before starting Drastic for the first time..." 2>&1
-
-    sleep 5
-    clear
-    exit 1
-  fi
-
 fi
 
 if [ ! -d "/storage/.config/drastic/aarch64/drastic/config" ]; then
