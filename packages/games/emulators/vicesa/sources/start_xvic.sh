4f46600defdf2d9b739f577dc032106dcac84a18
fewtarius <fewtarius@jelos.org>
2023-01-01 15:19:02 -0500

    Cleanup and bug fixes.

--- packages/games/emulators/vicesa/sources/start_xvic.sh
+++ packages/games/emulators/vicesa/sources/start_xvic.sh
@@ -12,4 +12,4 @@ fi
 ARG=${1//[\\]/}
 export SDL_AUDIODRIVER=alsa
 echo "[${ARG}]"
-${FAST_CORES} xvic -sounddev alsa -sdl2renderer opengles2 "$ARG"
+xvic -sounddev alsa -sdl2renderer opengles2 "$ARG"
