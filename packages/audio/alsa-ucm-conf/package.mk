92c10bc07277182675c8eec1ddddb1888ad1d26d
fewtarius <fewtarius@jelos.org>
2023-01-12 18:46:54 -0500

    Work on Steam Deck support.

--- packages/audio/alsa-ucm-conf/package.mk
+++ packages/audio/alsa-ucm-conf/package.mk
@@ -2,11 +2,9 @@
 # Copyright (C) 2020-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="alsa-ucm-conf"
-PKG_VERSION="1.2.6.3"
-PKG_SHA256="b8a03aa387a624a2f65edc201bf777421190b60529a92087646823afbd96c5cd"
+PKG_VERSION="1.2.8"
 PKG_LICENSE="BSD-3c"
 PKG_SITE="http://www.alsa-project.org/"
-#PKG_URL="ftp://ftp.alsa-project.org/pub/lib/alsa-ucm-conf-${PKG_VERSION}.tar.bz2"
 PKG_URL="https://www.alsa-project.org/files/pub/lib/alsa-ucm-conf-${PKG_VERSION}.tar.bz2"
 PKG_LONGDESC="ALSA Use Case Manager configuration (and topologies)"
 PKG_TOOLCHAIN="manual"
