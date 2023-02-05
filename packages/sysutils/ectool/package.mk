310025b34004343ccfca60d485c0dc6f0e9c1d8b
fewtarius <fewtarius@jelos.org>
2023-02-05 07:03:30 -0500

    Limit ectool to x86_64

--- packages/sysutils/ectool/package.mk
+++ packages/sysutils/ectool/package.mk
@@ -6,6 +6,7 @@ PKG_VERSION="4d661eebe95b06acae7d99777ed36ba56a560112"
 PKG_LICENSE="GPL"
 PKG_SITE="https://review.coreboot.org/coreboot"
 PKG_URL="${PKG_SITE}.git"
+PKG_ARCH="x86_64"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="A simple tool for manipulating the embedded controller."
 
