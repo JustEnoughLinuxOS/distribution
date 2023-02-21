9c1898c92b3fdb8e4dbb6e9f635e7bc30ab57367
amd343 <115344800+amd343@users.noreply.github.com>
2023-02-20 21:31:19 -0800

    Add pcsx_rearmed and flycast with 32bit

---
+++ packages/games/libretro/pcsx_rearmed/package.mk
@@ -0,0 +1,37 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2020 Trond Haugland (trondah@gmail.com)
+# Copyright (C) 2022-present Fewtarius
+
+PKG_NAME="pcsx_rearmed"
+PKG_VERSION="aced3eb3fcaa0fe13c44c4dd196cdab42555fd98"
+PKG_REV="1"
+PKG_ARCH="arm aarch64"
+PKG_LICENSE="GPLv2"
+PKG_SITE="https://github.com/libretro/pcsx_rearmed"
+PKG_URL="$PKG_SITE/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_SHORTDESC="ARM optimized PCSX fork"
+PKG_TOOLCHAIN="manual"
+PKG_PATCH_DIRS+="${TARGET_ARCH}/${DEVICE}"
+
+make_target() {
+  cd ${PKG_BUILD}
+  if [[ "${DEVICE}" =~ RK3566 ]]
+  then
+    make -f Makefile.libretro GIT_VERSION=${PKG_VERSION} platform=RK3566
+  else
+    make -f Makefile.libretro GIT_VERSION=${PKG_VERSION} platform=${DEVICE}
+  fi
+}
+
+makeinstall_target() {
+  ## Install the 64bit core.
+  mkdir -p ${INSTALL}/usr/lib/libretro
+  cp pcsx_rearmed_libretro.so ${INSTALL}/usr/lib/libretro/
+  if [ "${TARGET_ARCH}" = "aarch64" ]
+  then
+    ## Install the 32bit core.
+    cp -vP ${ROOT}/build.${DISTRO}-${DEVICE}.arm/pcsx_rearmed-*/.install_pkg/usr/lib/libretro/pcsx_rearmed_libretro.so ${INSTALL}/usr/lib/libretro/pcsx_rearmed32_libretro.so
+  fi
+}
+
