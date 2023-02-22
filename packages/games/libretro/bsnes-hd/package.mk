0ef76e011837a73b18040e0ba0c0087bd2a8aef7
daedalia <bryn@daedalia.co.uk>
2023-02-22 09:36:27 +0000

    bsnes-hd - License addition and remove redundant script

--- packages/games/libretro/bsnes-hd/package.mk
+++ packages/games/libretro/bsnes-hd/package.mk
@@ -1,3 +1,6 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (C) 2023-present Daedalia (https://github.com/daedalia)
+
 PKG_NAME="bsnes-hd"
 PKG_VERSION="04821703aefdc909a4fd66d168433fcac06c2ba7"
 PKG_LICENSE="GPLv2"
@@ -14,11 +17,6 @@ PKG_AUTORECONF="no"
 PKG_MAKE_OPTS_TARGET+=" -C bsnes target=libretro compiler=${TARGET_NAME}-g++"
 
 makeinstall_target() {
-    case ${ARCH} in
-      aarch64|arm)
-        wget https://raw.githubusercontent.com/libretro/libretro-super/master/retrolink.sh && chmod +x retrolink.sh && ./retrolink.sh bsnes/out/bsnes_hd_beta_libretro.so
-      ;;
-    esac
   mkdir -p $INSTALL/usr/lib/libretro
   cp bsnes/out/bsnes_hd_beta_libretro.so $INSTALL/usr/lib/libretro/
 }
