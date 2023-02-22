dfc9ea6257e448e450750ab169f537eec79feeb2
fewtarius <fewtarius@jelos.org>
2023-02-22 07:22:43 -0500

    Add apache-ant work around as ftp server is unavailable.

--- packages/devel/apache-ant/package.mk
+++ packages/devel/apache-ant/package.mk
@@ -3,7 +3,7 @@
 # Copyright (C) 2020-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="apache-ant"
-PKG_VERSION="1.10.12"
+PKG_VERSION="1.10.13"
 PKG_LICENSE="Apache License 2.0"
 PKG_SITE="https://ant.apache.org/"
 PKG_URL="https://archive.apache.org/dist/ant/source/${PKG_NAME}-${PKG_VERSION}-src.tar.xz"
@@ -14,6 +14,18 @@ PKG_TOOLCHAIN="manual"
 make_host() {
   (
   export JAVA_HOME=$(get_build_dir jdk-zulu)
+
+   ### Work around for down/missing ftp server.
+   TMPCACHE="${HOME}/.ant/tempcache"
+   if [ ! -d "${TMPCACHE}" ]
+   then
+     mkdir -p ${TMPCACHE}
+   fi
+
+   if [ ! -e "${TMPCACHE}/NetRexx.zip" ]
+   then
+     curl -Lo ${TMPCACHE}/NetRexx.zip https://public.dhe.ibm.com/software/awdtools/netrexx/
+   fi
 
   ./bootstrap.sh
   ./bootstrap/bin/ant -f fetch.xml -Ddest=optional
