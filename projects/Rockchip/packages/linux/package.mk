17317a81abdc566318eb8c3d02ee6dd9dc24d037
fewtarius <fewtarius@jelos.org>
2023-02-22 16:54:01 -0500

    * Fix battery reporting on RK3566 devices. * Update Linux kernel to 6.1.13. * Update PPSSPP. * Fix jelos-info hang if temp device doesn't exist.

--- projects/Rockchip/packages/linux/package.mk
+++ projects/Rockchip/packages/linux/package.mk
@@ -26,7 +26,7 @@ case ${DEVICE} in
   ;;
   RK3566)
     PKG_URL="${PKG_SITE}/rk356x-kernel.git"
-    PKG_VERSION="96accfa77"
+    PKG_VERSION="22a0a3344"
     GET_HANDLER_SUPPORT="git"
     PKG_GIT_CLONE_BRANCH="main"
   ;;
