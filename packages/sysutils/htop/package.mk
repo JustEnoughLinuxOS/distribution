1385a756a7d2ed068e31252881007b3e595ed0d1
fewtarius <fewtarius@jelos.org>
2023-03-01 06:23:27 -0500

    Update/fix htop.

--- packages/sysutils/htop/package.mk
+++ packages/sysutils/htop/package.mk
@@ -2,11 +2,10 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="htop"
-PKG_VERSION="3.0.0beta5"
-PKG_SHA256="c439add8d6a463699629fc3f0103f55b045d519c31611fa4a68629063238985a"
+PKG_VERSION="55c10ec" # 3.2.2
 PKG_LICENSE="GPL"
-PKG_SITE="https://hisham.hm/htop"
-PKG_URL="https://github.com/hishamhm/htop/archive/${PKG_VERSION}.tar.gz"
+PKG_SITE="https://github.com/htop-dev/htop"
+PKG_URL="${PKG_SITE}.git"
 PKG_DEPENDS_TARGET="toolchain ncurses"
 PKG_LONGDESC="An interactive process viewer for Unix."
 PKG_TOOLCHAIN="autotools"
@@ -14,7 +13,3 @@ PKG_TOOLCHAIN="autotools"
 PKG_CONFIGURE_OPTS_TARGET="--disable-unicode \
                            HTOP_NCURSES_CONFIG_SCRIPT=ncurses6-config"
 
-pre_configure_target() {
-  export LDFLAGS="${LDFLAGS} -lreadline -lncursesw -ltinfow"
-  export CFLAGS="${CFLAGS} -fcommon"
-}
