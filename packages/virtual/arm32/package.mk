dd333c21f1fbe0e872a8f5aa84c67d3e558dac4c
fewtarius <fewtarius@jelos.org>
2023-03-04 08:43:47 -0500

    Work continues on 32bit compat support.

--- packages/virtual/arm32/package.mk
+++ packages/virtual/arm32/package.mk
@@ -4,6 +4,27 @@
 PKG_NAME="arm32"
 PKG_LICENSE="Apache-2.0"
 PKG_SITE="www.jelos.org"
-PKG_DEPENDS_TARGET="toolchain squashfs-tools:host dosfstools:host fakeroot:host kmod:host mtools:host populatefs:host libc gcc linux linux-drivers linux-firmware libusb unzip socat p7zip file ${OPENGLES} SDL2 SDL2_gfx SDL2_image SDL2_mixer SDL2_net SDL2_ttf retroarch pcsx_rearmed flycast box86"
+PKG_DEPENDS_TARGET="toolchain squashfs-tools:host dosfstools:host fakeroot:host kmod:host mtools:host populatefs:host libc gcc linux linux-drivers linux-firmware libusb unzip socat p7zip file SDL2 SDL2_gfx SDL2_image SDL2_mixer SDL2_net SDL2_ttf"
 PKG_SECTION="virtual"
 PKG_LONGDESC="Root package used to build and create 32-bit userland"
+
+### Display Drivers
+if [ ! "${OPENGL}" = "no" ]; then
+  PKG_DEPENDS_TARGET+=" ${OPENGL} glu libglvnd glew"
+fi
+
+if [ "${OPENGLES_SUPPORT}" = yes ]; then
+  PKG_DEPENDS_TARGET+=" ${OPENGLES}"
+fi
+
+if [ "${VULKAN_SUPPORT}" = "yes" ]
+then
+  PKG_DEPENDS_TARGET+=" vulkan-loader vulkan-headers"
+fi
+
+if [ "${DISPLAYSERVER}" = "wl" ]; then
+  PKG_DEPENDS_TARGET+=" wayland ${WINDOWMANAGER}"
+fi
+
+### Emulators and Cores
+PKG_DEPENDS_TARGET+=" retroarch pcsx_rearmed flycast box86"
