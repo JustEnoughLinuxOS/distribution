ad28cc5065c51da05cd468b485d918165ec6f73b
fewtarius <fewtarius@jelos.org>
2023-03-09 09:12:11 -0500

    Fix bluetooth.

--- packages/devel/glib/package.mk
+++ packages/devel/glib/package.mk
@@ -3,8 +3,7 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="glib"
-PKG_VERSION="2.75.2"
-PKG_SHA256="360d6fb75202c0eb0d07f0ab812b19b526f1c05ccc0a8ed7e5d2c988616d343a"
+PKG_VERSION="2.75.4"
 PKG_LICENSE="LGPL"
 PKG_SITE="https://www.gtk.org/"
 PKG_URL="https://download.gnome.org/sources/glib/$(get_pkg_version_maj_min)/${PKG_NAME}-${PKG_VERSION}.tar.xz"
--- packages/python/devel/pygobject/package.mk
+++ packages/python/devel/pygobject/package.mk
@@ -4,28 +4,24 @@
 # Copyright (C) 2022-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="pygobject"
-PKG_VERSION="3.42.2"
-PKG_SHA256="ade8695e2a7073849dd0316d31d8728e15e1e0bc71d9ff6d1c09e86be52bc957"
+PKG_VERSION="3.43.1"
 PKG_ARCH="any"
 PKG_LICENSE="LGPL"
 PKG_SITE="http://www.pygtk.org/"
 PKG_URL="http://ftp.gnome.org/pub/GNOME/sources/pygobject/$(get_pkg_version_maj_min)/${PKG_NAME}-${PKG_VERSION}.tar.xz"
 PKG_DEPENDS_TARGET="toolchain Python3 glib libffi gobject-introspection pgi"
 PKG_LONGDESC="A convenient wrapper for the GObject+ library for use in Python programs."
-PKG_TOOLCHAIN="meson"
+
+pre_configure_target() {
+  export PYTHONXCPREFIX="${SYSROOT_PREFIX}/usr"
+  export LDSHARED="${CC} -shared"
+}
 
 pre_configure_target() {
   PKG_CONFIG_PATH="${SYSROOT_PREFIX}/usr/lib/pkgconfig"
-
   PKG_MESON_OPTS_TARGET=" \
-    -Dpython=${TOOLCHAIN}/bin/${PKG_PYTHON_VERSION} \
-    -Dpycairo=disabled \
-    -Dtests=false"
+                         -Dpython=${TOOLCHAIN}/bin/${PKG_PYTHON_VERSION} \
+                         -Dpycairo=disabled \
+                         -Dtests=false"
 }
 
-post_makeinstall_target() {
-  python_remove_source
-
-  rm -rf ${INSTALL}/usr/bin
-  rm -rf ${INSTALL}/usr/share/pygobject
-}
