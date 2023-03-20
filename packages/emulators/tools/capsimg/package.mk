bcaf516e09af8599c58186868b72f5254b3417b6
fewtarius <fewtarius@jelos.org>
2023-03-20 10:54:45 +0000

    Update capsimg, ensure that kernel drivers are rebuilt when the kernel is updated.

--- packages/emulators/tools/capsimg/package.mk
+++ packages/emulators/tools/capsimg/package.mk
@@ -2,11 +2,10 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="capsimg"
-PKG_VERSION="457d420848145de6541e47296405b1938830900a"
-PKG_SHA256="d32c4d802301e33abef067837a20bc8bc5d9769154820b99408c6a967e8cd402"
+PKG_VERSION="ed4cb0912fb1a2a0d5203111c6f6abd50d411b56"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/FrodeSolheim/capsimg"
-PKG_URL="https://github.com/FrodeSolheim/capsimg/archive/${PKG_VERSION}.tar.gz"
+PKG_URL="${PKG_SITE}.git"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="SPS Decoder Library 5.1 (formerly IPF Decoder Lib)"
 PKG_TOOLCHAIN="make"
@@ -25,6 +24,6 @@ makeinstall_target() {
   cp -v  CAPSImg/libcapsimage.so.5.1 ${INSTALL}/usr/lib/
   ln -sf libcapsimage.so.5.1 ${INSTALL}/usr/lib/libcapsimage.so.5
   ln -sf libcapsimage.so.5.1 ${INSTALL}/usr/lib/libcapsimage.so
-  mkdir -p ${INSTALL}/usr/local/include/caps5
-  cp -v LibIPF/* ${INSTALL}/usr/local/include/caps5
+  mkdir -p ${INSTALL}/usr/include/caps5
+  cp -v LibIPF/* ${INSTALL}/usr/include/caps5
 }
