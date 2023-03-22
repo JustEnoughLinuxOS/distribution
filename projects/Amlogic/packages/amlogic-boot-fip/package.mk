d25b13265285a1f7e444aeb205113cd4ce32a2e8
mason <macebrooks@gmail.com>
2023-03-22 21:56:33 +0000

    Initial OGU work, kernel boots

---
+++ projects/Amlogic/packages/amlogic-boot-fip/package.mk
@@ -0,0 +1,11 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2023-present BrooksyTech (https://github.com/brooksytech)
+
+PKG_NAME="amlogic-boot-fip"
+PKG_VERSION="e96b6a694380ff07d5a9e4be644ffe254bd18512"
+PKG_LICENSE="GPLv3"
+PKG_SITE="https://github.com/LibreELEC/amlogic-boot-fip"
+PKG_URL="${PKG_SITE}.git"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_SHORTDESC="Firmware Image Pacakge (FIP) sources used to sign Amlogic u-boot binaries"
+PKG_TOOLCHAIN="manual"
