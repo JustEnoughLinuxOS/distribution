ec5a26d72a8c19ae29eca09ccec9385ff275c3ed
mason <macebrooks@gmail.com>
2023-03-30 06:00:51 +0000

    Fix Retroarch, add controller configs

--- projects/Amlogic/packages/linux/package.mk
+++ projects/Amlogic/packages/linux/package.mk
@@ -19,7 +19,7 @@ PKG_PATCH_DIRS+="${DEVICE}"
 
 case ${DEVICE} in
   OGU)
-    PKG_VERSION="525f871874b3bbb724a333f45a4ae2002a201329"
+    PKG_VERSION="ebbf6b19d27cd2a11be22c0282a7bbfdab64bbe3"
     PKG_URL="https://github.com/mdrjr/linux.git"
     GET_HANDLER_SUPPORT="git"
     PKG_GIT_CLONE_BRANCH="odroidg12-6.1.y"
