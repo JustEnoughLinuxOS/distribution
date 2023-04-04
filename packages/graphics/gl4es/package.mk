b31083df536e56f3fdc35bc7f67a30f8610c6f2e
mason <macebrooks@gmail.com>
2023-04-04 13:38:06 +0000

    S922X cleanup

--- packages/graphics/gl4es/package.mk
+++ packages/graphics/gl4es/package.mk
@@ -4,20 +4,34 @@
 # Copyright (C) 2023-present Fewtarius
 
 PKG_NAME="gl4es"
-PKG_VERSION="57240b0f06551a49c94f90abccc54767c738791e"
-PKG_SITE="https://github.com/ptitSeb/gl4es"
 PKG_LICENSE="GPL"
 PKG_URL="${PKG_SITE}.git"
 PKG_DEPENDS_TARGET="toolchain ${OPENGLES}"
 PKG_LONGDESC="OpenGL 2.x functionality for GLES2.0 accelerated Hardware"
 PKG_TOOLCHAIN="cmake-make"
 
+case ${DEVICE} in
+  S922X)
+    PKG_SITE="https://github.com/JohnnyonFlame/gl4es"
+    PKG_URL="${PKG_SITE}.git"
+    PKG_VERSION="17a7876ae76eb7b50ff375d80abd35018e58a50d"
+    PKG_GIT_CLONE_BRANCH="ge2d"
+  ;;
+  *)
+    PKG_SITE="https://github.com/ptitSeb/gl4es"
+    PKG_URL="${PKG_SITE}.git"
+    PKG_VERSION="22eaac79757b0e4fe011deef9cc0596ba35c22b0"
+  ;;
+esac
+
+
 pre_configure_target() {
   PKG_CMAKE_OPTS_TARGET=" -DGBM=ON -DCMAKE_BUILD_TYPE=Release "
   if [ ! "${DISPLAYSERVER}" = "x11" ]
   then
-    PKG_CMAKE_OPTS_TARGET+=" -DNOX11=1"
+    PKG_CMAKE_OPTS_TARGET+=" -DNOX11=ON"
   else
     PKG_DEPENDS_TARGET+=" libX11"
   fi
+    PKG_CMAKE_OPTS_TARGET+=" -DGLX_STUBS=ON -DEGL_WRAPPER=ON -DGBM=ON"
 }
