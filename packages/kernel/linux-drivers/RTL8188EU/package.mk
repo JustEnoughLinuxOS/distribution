ee5a5a56679e4cbdf85a12450a94b9c45d9f7567
mason <macebrooks@gmail.com>
2023-04-11 13:27:26 +0000

    Fix occasional build errors on  wifi drivers

--- packages/kernel/linux-drivers/RTL8188EU/package.mk
+++ packages/kernel/linux-drivers/RTL8188EU/package.mk
@@ -11,6 +11,7 @@ PKG_DEPENDS_TARGET="toolchain linux kernel-firmware"
 PKG_NEED_UNPACK="${LINUX_DEPENDS}"
 PKG_LONGDESC="Realtek RTL81xxEU Linux 3.x driver"
 PKG_IS_KERNEL_PKG="yes"
+PKG_TOOLCHAIN="make"
 
 pre_make_target() {
   unset LDFLAGS
