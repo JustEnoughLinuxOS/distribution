94a10d1126fb2caf048342a7b22532a6aa61fbbc
fewtarius <fewtarius@jelos.org>
2023-04-16 00:17:16 +0000

    Re-add mupen64plus-sa to RK3566 devices.

--- packages/virtual/emulators/package.mk
+++ packages/virtual/emulators/package.mk
@@ -43,7 +43,7 @@ case "${DEVICE}" in
   ;;
   RK3566)
     [ "${ENABLE_32BIT}" == "true" ] && EMUS_32BIT="box86 flycast-lr pcsx_rearmed-lr"
-    PKG_DEPENDS_TARGET+=" common-shaders glsl-shaders box64"
+    PKG_DEPENDS_TARGET+=" common-shaders glsl-shaders mupen64plus-sa box64"
     PKG_EMUS+=" yabasanshiro-sa"
   ;;
   S922X)
