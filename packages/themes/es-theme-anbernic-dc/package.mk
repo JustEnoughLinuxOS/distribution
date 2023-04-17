a19f55b6ac8f29c9ddffcf01b86b00100d16ef7c
LittleBoi <byunjaeil@gmail.com>
2023-04-17 09:38:48 +0900

    Add files via upload

--- packages/themes/es-theme-anbernic-dc/package.mk
+++ packages/themes/es-theme-anbernic-dc/package.mk
@@ -1,1 +1,16 @@
+#UzuCore
 
+PKG_NAME="es-theme-anbernic-dc"
+PKG_VERSION="053f5a73f5db09537c0fca1fdd9fb1c58c2e1f10"
+PKG_ARCH="any"
+PKG_LICENSE="CUSTOM"
+PKG_SITE="https://github.com/UzuCore/es-theme-anbernic-dc"
+PKG_URL="${PKG_SITE}.git"
+PKG_SHORTDESC="Anbernic Epic Noir"
+PKG_LONGDESC="Anbernic Epic Noir"
+PKG_TOOLCHAIN="manual"
+
+makeinstall_target() {
+    mkdir -p ${INSTALL}/usr/config/emulationstation/themes/${PKG_NAME}
+    cp -rf * ${INSTALL}/usr/config/emulationstation/themes/${PKG_NAME}
+}
