c59014faca07a48a578671217f66aa3d64dfbeba
fewtarius <fewtarius@jelos.org>
2023-04-23 19:58:11 +0000

    Drop image splash screens.

--- packages/graphics/imagemagick/package.mk
+++ packages/graphics/imagemagick/package.mk
@@ -7,7 +7,7 @@ PKG_VERSION="920f792"
 PKG_LICENSE="http://www.imagemagick.org/script/license.php"
 PKG_SITE="https://github.com/ImageMagick/ImageMagick"
 PKG_URL="${PKG_SITE}.git"
-PKG_DEPENDS_TARGET="toolchain libpng librsvg libjpeg-turbo fontconfig libraw lcms2 libtool libxml2 libzip"
+PKG_DEPENDS_TARGET="toolchain libpng libjpeg-turbo fontconfig libraw lcms2 libtool libxml2 libzip"
 PKG_LONGDESC="Software suite to create, edit, compose, or convert bitmap images"
 
 PKG_CONFIGURE_OPTS_TARGET="--disable-openmp \
