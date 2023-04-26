a8e2776438523796275de9ca9c18cb6bc44d96be
byunjaeil <byunjaeil@gmail.com>
2023-04-26 16:25:21 +0900

    fix?

--- packages/wayland/wayland-protocols/package.mk
+++ packages/wayland/wayland-protocols/package.mk
@@ -2,11 +2,10 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="wayland-protocols"
-PKG_VERSION="1.26"
-PKG_SHA256="c553384c1c68afd762fa537a2569cc9074fe7600da12d3472761e77a2ba56f13"
+PKG_VERSION="cc0cd4addf68df7a14bf7f4c9c2daa6489fc20d7"
 PKG_LICENSE="OSS"
-PKG_SITE="https://wayland.freedesktop.org/"
-PKG_URL="https://wayland.freedesktop.org/releases/${PKG_NAME}-${PKG_VERSION}.tar.xz"
+PKG_SITE="https://gitlab.freedesktop.org/wayland/wayland-protocols"
+PKG_URL="${PKG_SITE}.git"
 PKG_DEPENDS_TARGET="toolchain wayland:host"
 PKG_LONGDESC="Specifications of extended Wayland protocols"
 
