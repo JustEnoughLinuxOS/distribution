7a4e5b09caa79d773e2ee5ec0156a86d5ed78124
byunjaeil <byunjaeil@gmail.com>
2023-04-27 04:31:41 +0900

    Update package.mk

--- packages/virtual/emulators/package.mk
+++ packages/virtual/emulators/package.mk
@@ -44,10 +44,9 @@ case "${DEVICE}" in
     PKG_RETROARCH+=" retropie-shaders"
   ;;
   RK356*)
-    [ "${ENABLE_32BIT}" == "true" ] && EMUS_32BIT="box86 flycast-lr pcsx_rearmed-lr"
+    [ "${ENABLE_32BIT}" == "true" ] && EMUS_32BIT="box86 flycast-lr flycast2021-lr pcsx_rearmed-lr"
     PKG_DEPENDS_TARGET+=" common-shaders glsl-shaders mupen64plus-sa box64"
     PKG_EMUS+=" dolphin-sa drastic-sa yabasanshiro-sa"
-    LIBRETRO_CORES+=" flycast2021-lr"
     PKG_RETROARCH+=" retropie-shaders"
   ;;
   S922X)
