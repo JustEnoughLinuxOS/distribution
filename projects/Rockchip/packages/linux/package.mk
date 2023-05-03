4d2786212a2f8582d65ef686ecbb8ad092bbf57a
fewtarius <fewtarius@jelos.org>
2023-05-03 12:10:46 +0000

    Revert v1 timings, should correct panel output for some 353p users.

--- projects/Rockchip/packages/linux/package.mk
+++ projects/Rockchip/packages/linux/package.mk
@@ -26,7 +26,7 @@ case ${DEVICE} in
   ;;
   RK3566)
     PKG_URL="${PKG_SITE}/rk356x-kernel.git"
-    PKG_VERSION="5aef2e6"
+    PKG_VERSION="5bab31384"
     GET_HANDLER_SUPPORT="git"
     PKG_GIT_CLONE_BRANCH="main"
   ;;
