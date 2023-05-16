a0572dfe42fb7c200b06945638c25b1b8ae0c4b2
fewtarius <fewtarius@jelos.org>
2023-05-16 10:18:00 +0000

    Add ZeroTier support to ES, thanks to @NeoTheFox.

--- packages/ui/emulationstation/package.mk
+++ packages/ui/emulationstation/package.mk
@@ -3,7 +3,7 @@
 # Copyright (C) 2020-present Fewtarius
 
 PKG_NAME="emulationstation"
-PKG_VERSION="1eadb90edf04d40b2f92307e7767334975234c55"
+PKG_VERSION="c0d963d"
 PKG_GIT_CLONE_BRANCH="main"
 PKG_REV="1"
 PKG_ARCH="any"
