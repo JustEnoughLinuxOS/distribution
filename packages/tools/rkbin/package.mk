38ac78237cffb33eafd6615ad39a16568a86d0cc
byunjaeil <byunjaeil@gmail.com>
2023-05-24 13:19:22 +0900

    Update package.mk

--- packages/tools/rkbin/package.mk
+++ packages/tools/rkbin/package.mk
@@ -1,6 +1,3 @@
-# SPDX-License-Identifier: GPL-2.0
-# Copyright (C) 2023-present BrooksyTech
-
 PKG_NAME="rkbin"
 PKG_ARCH="arm aarch64"
 PKG_LICENSE="nonfree"
@@ -9,5 +6,13 @@ PKG_LONGDESC="rkbin: Rockchip Firmware and Tool Binaries"
 PKG_TOOLCHAIN="manual"
 PKG_PATCH_DIRS+="${DEVICE}*"
 
-PKG_VERSION="7f77e406fd1644967301bafb6bec71cf3c6cbb05"
-PKG_URL="${PKG_SITE}.git"
+case ${DEVICE} in
+  RK3326)
+    PKG_VERSION="7f77e406fd1644967301bafb6bec71cf3c6cbb05"
+    PKG_URL="${PKG_SITE}.git"
+  ;;
+  *)
+    PKG_VERSION="9840e87723eef7c41235b89af8c049c1bcd3d133"
+    PKG_URL="${PKG_SITE}.git"
+  ;;
+esac
