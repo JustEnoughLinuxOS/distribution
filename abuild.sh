1512047651e4bbb6ca22d5caaaf757c3f741a500
byunjaeil <byunjaeil@gmail.com>
2023-05-31 14:03:58 +0900

    !!!?

--- abuild.sh
+++ abuild.sh
@@ -8,6 +8,7 @@ else
   PROJECT=Rockchip DEVICE=RK3588 ARCH=aarch64 ./scripts/build $1
   PROJECT=Rockchip DEVICE=RK3326 ARCH=aarch64 ./scripts/build $1
   PROJECT=Rockchip DEVICE=RK3566 ARCH=aarch64 ./scripts/build $1
+  PROJECT=Rockchip DEVICE=RK3566-X55 ARCH=aarch64 ./scripts/build $1
   PROJECT=Amlogic DEVICE=S922X ARCH=aarch64 ./scripts/build $1
   echo "Yeah-"
 fi
