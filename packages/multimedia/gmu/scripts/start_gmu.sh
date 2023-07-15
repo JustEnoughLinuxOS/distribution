28fdff65f3748ef47ceff4083ffefc5c5626bf4b
fewtarius <fewtarius@jelos.org>
2023-07-15 23:43:20 +0000

    Fix for small displays.

--- packages/multimedia/gmu/scripts/start_gmu.sh
+++ packages/multimedia/gmu/scripts/start_gmu.sh
@@ -6,12 +6,25 @@
 
 jslisten set "-9 gmu.bin"
 
-GMUCONFIG="/storage/.config/gmu/gmu.conf"
+GMUPATH="/storage/.config/gmu"
+GMUCONFIG="${GMUPATH}/gmu.conf"
 
 FBHEIGHT=$(fbset | awk '/geometry/ {print $2}')
 FBWIDTH=$(fbset | awk '/geometry/ {print $3}')
+
+if [ ! -d "${GMUPATH}" ]
+then
+  cp -rf /usr/config/gmu ${GMUPATH}
+fi
+
 sed -i "s~SDL.Height=.*\$~SDL.Height=${FBHEIGHT}~g" ${GMUCONFIG}
-sed -i "s~SDL.Width=.*\$~SDL.Height=${FBWIDTH}~g" ${GMUCONFIG}
+sed -i "s~SDL.Width=.*\$~SDL.Width=${FBWIDTH}~g" ${GMUCONFIG}
+
+if (( ${FBWIDTH} <= 800 ))
+then
+  sed -i "s~default-modern-large~default-modern~g" ${GMUCONFIG}
+  sed -i "s~SDL.Fullscreen=.*\$~SDL.Fullscreen=no~g" ${GMUCONFIG}
+fi
 
 cd /usr/local/share/gmu
 /usr/local/bin/gmu.bin -d /usr/local/etc/gmu -c /storage/.config/gmu/gmu.conf
