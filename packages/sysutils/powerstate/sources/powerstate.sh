038f05985b99efcee4a63c32bf8e8f059b22a7ad
fewtarius <fewtarius@jelos.org>
2023-07-27 13:52:57 +0000

    Fix oops.

--- packages/sysutils/powerstate/sources/powerstate.sh
+++ packages/sysutils/powerstate/sources/powerstate.sh
@@ -49,7 +49,7 @@ do
           gpu_performance_level auto
           pcie_aspm_policy default
           wake_events disabled
-          runtime_power_management off
+          runtime_power_management on
           /usr/bin/wifictl setpowersave
         ;;
       esac
