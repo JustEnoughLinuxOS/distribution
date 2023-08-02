f921a1277532965479237c10809c1b06c6ad69d4
fewtarius <fewtarius@jelos.org>
2023-08-02 22:56:43 +0000

    Fix mupen64plus sa on remaining devices.

--- packages/emulators/standalone/mupen64plus-sa/mupen64plus-sa-audio-sdl/package.mk
+++ packages/emulators/standalone/mupen64plus-sa/mupen64plus-sa-audio-sdl/package.mk
@@ -14,29 +14,27 @@ PKG_LONGDESC="Mupen64Plus Standalone Audio SDL"
 PKG_TOOLCHAIN="manual"
 
 case ${DEVICE} in
-  AMD64|RK3588|S922X|RK3399)
+  AMD64)
     PKG_DEPENDS_TARGET+=" mupen64plus-sa-simplecore"
   ;;
 esac
 
-if [ "${OPENGL_SUPPORT}" = "yes" ]
-then
-  PKG_DEPENDS_TARGET+=" ${OPENGL} glu libglvnd"
-else
-  PKG_DEPENDS_TARGET+=" ${OPENGLES}"
-fi
+case ${DEVICE} in
+  AMD64|RK33*|RK3588)
+    PKG_DEPENDS_TARGET+=" ${OPENGL} glu libglvnd"
+    export USE_GLES=0
+  ;;
+  *)
+    PKG_DEPENDS_TARGET+=" ${OPENGLES}"
+    export USE_GLES=1
+  ;;
+esac
 
 make_target() {
-  if [ "${OPENGL_SUPPORT}" = "yes" ]
-  then
-    export USE_GLES=0
-  else
-    export USE_GLES=1
-  fi
 
   export HOST_CPU=${TARGET_ARCH} \
-         VFP_HARD=1 \
          NEW_DYNAREC=1 \
+         VFP_HARD=1 \
          V=1 \
          VC=0 \
          OSD=0
