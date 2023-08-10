34a7726efc7c344cc396ca4e003a257426d12dd0
fewtarius <fewtarius@jelos.org>
2023-08-10 16:23:00 +0000

    * Fix Bluetooth audio across all ARM devices. * Fix scummvm standalone audio issues.

--- packages/audio/fluidsynth/package.mk
+++ packages/audio/fluidsynth/package.mk
@@ -3,12 +3,11 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="fluidsynth"
-PKG_VERSION="2.2.3"
-PKG_SHA256="b31807cb0f88e97f3096e2b378c9815a6acfdc20b0b14f97936d905b536965c4"
+PKG_VERSION="2.3.3"
 PKG_LICENSE="GPL"
 PKG_SITE="http://fluidsynth.org/"
 PKG_URL="https://github.com/FluidSynth/fluidsynth/archive/v${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain glib libsndfile"
+PKG_DEPENDS_TARGET="toolchain glib libsndfile pulseaudio pipewire"
 PKG_LONGDESC="FluidSynth renders midi music files as raw audio data, for playing or conversion."
 PKG_BUILD_FLAGS="+pic"
 
@@ -16,5 +15,7 @@ PKG_CMAKE_OPTS_TARGET="-DBUILD_SHARED_LIBS=0 \
                        -DLIB_SUFFIX= \
                        -Denable-libsndfile=1 \
                        -Denable-pkgconfig=1 \
-                       -Denable-pulseaudio=0 \
+                       -Denable-pipewire=1 \
+                       -Denable-pulseaudio=1 \
+                       -Denable-systemd=1 \
                        -Denable-readline=0"
