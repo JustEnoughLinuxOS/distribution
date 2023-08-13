a1258e1ece83c2f0fed52207c9b33f9724455cbe
mason <macebrooks@gmail.com>
2023-08-13 12:43:30 +0000

    Add support for NanoBoyAdance SA

---
+++ packages/emulators/standalone/nanoboyadvance-sa/package.mk
@@ -0,0 +1,36 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2023-present BrooksyTech (https://github.com/brooksytech)
+
+PKG_NAME="nanoboyadvance-sa"
+PKG_VERSION="3bb6f478f977dbfd3106508536e5fbce90d1898b"
+PKG_LICENSE="GPLv3"
+PKG_SITE="https://github.com/nba-emu/NanoBoyAdvance"
+PKG_URL="${PKG_SITE}.git"
+PKG_OPEN_SOURCE_BIOS="https://github.com/Nebuleon/ReGBA/raw/master/bios/gba_bios.bin"
+PKG_DEPENDS_TARGET="toolchain SDL2 glew"
+PKG_LONGDESC="NanoBoyAdvance is a cycle-accurate Game Boy Advance emulator."
+PKG_TOOLCHAIN="cmake"
+PKG_PATCH_DIRS+="${DEVICE}"
+
+if [ "${OPENGL_SUPPORT}" = "yes" ]; then
+  PKG_DEPENDS_TARGET+=" ${OPENGL} glu libglvnd"
+elif [ "${OPENGLES_SUPPORT}" = yes ]; then
+  PKG_DEPENDS_TARGET+=" ${OPENGLES}"
+fi
+
+PKG_CMAKE_OPTS_TARGET+=" -DPLATFORM_SDL2=ON \
+                         -DPLATFORM_QT=OFF"
+
+makeinstall_target() {
+  mkdir -p ${INSTALL}/usr/bin
+  cp -rf ${PKG_BUILD}/.${TARGET_NAME}/bin/sdl/NanoBoyAdvance ${INSTALL}/usr/bin
+  cp -rf ${PKG_DIR}/scripts/* ${INSTALL}/usr/bin
+  chmod 755 ${INSTALL}/usr/bin/*
+
+  mkdir -p ${INSTALL}/usr/config/nanoboyadvance
+  cp -rf ${PKG_DIR}/config/config.toml ${INSTALL}/usr/config/nanoboyadvance/
+  cp -rf ${PKG_DIR}/config/${DEVICE}/keymap.toml ${INSTALL}/usr/config/nanoboyadvance/
+
+  mkdir -p ${INSTALL}/usr/config/nanoboyadvance/bios
+  curl -Lo ${INSTALL}/usr/config/nanoboyadvance/bios/gba_bios.bin ${PKG_OPEN_SOURCE_BIOS}
+}
---
+++ packages/emulators/standalone/nanoboyadvance-sa/scripts/start_nanoboyadvance.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2023-present BrooksyTech (https://github.com/brooksytech)
+
+. /etc/profile
+jslisten set "-9 NanoBoyAdvance"
+
+#Check if nanoboyadvance exists in .config
+if [ ! -d "/storage/.config/nanoboyadvance" ]; then
+    mkdir -p "/storage/.config/nanoboyadvance"
+        cp -r "/usr/config/nanoboyadvance" "/storage/.config/"
+fi
+
+#Make nanoboyadvance bios folder
+if [ ! -d "/storage/roms/bios/gba" ]; then
+    mkdir -p "/storage/roms/bios/gba"
+fi
+
+#Copy open source bios if no other bios exists
+if [ ! -f "/storage/roms/bios/gba/gba_bios.bin" ]; then
+  cp -r "/usr/config/nanoboyadvance" "/storage/roms/bios/gba/gba_bios.bin"
+fi
+
+#Set the cores to use
+CORES=$(get_setting "cores" "${PLATFORM}" "${ROMNAME##*/}")
+if [ "${CORES}" = "little" ]
+then
+  EMUPERF="${SLOW_CORES}"
+elif [ "${CORES}" = "big" ]
+then
+  EMUPERF="${FAST_CORES}"
+else
+  ### All..
+  unset EMUPERF
+fi
+
+#Run nanoboyadvance emulator
+${EMUPERF} /usr/bin/NanoBoyAdvance "${1}"
