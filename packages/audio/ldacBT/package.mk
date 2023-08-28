8f15720ca2b5e13374f6ea5193940ad5896660de
fewtarius <fewtarius@jelos.org>
2023-08-28 19:33:59 +0000

    Fix ldacBT.

--- packages/audio/ldacBT/package.mk
+++ packages/audio/ldacBT/package.mk
@@ -2,11 +2,10 @@
 # Copyright (C) 2021-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="ldacBT"
-PKG_VERSION="2.0.2.3"
-PKG_SHA256="c02998718f9c4620437d7594b4d121b3ab4c5cfeba8d41fa31dd5c71db09edca"
+PKG_VERSION="fbffba4"
 PKG_LICENSE="Apache"
 PKG_SITE="https://github.com/EHfive/ldacBT"
-PKG_URL="https://github.com/EHfive/ldacBT/archive/v${PKG_VERSION}.tar.gz"
+PKG_URL="${PKG_SITE}.git"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_DEPENDS_UNPACK="libldac"
 PKG_LONGDESC="LDAC Bluetooth encoder library (build tools)"
