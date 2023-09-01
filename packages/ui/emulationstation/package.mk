73e8f4029d7b42c29d3647650f8e1c1a97a6b358
fewtarius <fewtarius@jelos.org>
2023-09-01 13:08:48 +0000

    Update ES to the latest commit.

--- packages/ui/emulationstation/package.mk
+++ packages/ui/emulationstation/package.mk
@@ -3,7 +3,7 @@
 # Copyright (C) 2020-present Fewtarius
 
 PKG_NAME="emulationstation"
-PKG_VERSION="1d18e38909e87a4da9e97b038401d020625647a2"
+PKG_VERSION="48a334a"
 PKG_GIT_CLONE_BRANCH="main"
 PKG_REV="1"
 PKG_ARCH="any"
