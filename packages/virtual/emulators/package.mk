c3b82bbf7dd57fcf52424bf1fcba4d490f8d328e
byunjaeil <byunjaeil@gmail.com>
2023-10-07 06:26:50 +0900

    Update package.mk

--- packages/virtual/emulators/package.mk
+++ packages/virtual/emulators/package.mk
@@ -455,7 +455,7 @@ makeinstall_target() {
       add_emu_core gamecube primehack primehack false
       add_es_system gamecube
     ;;
-    S922X*|RK3399|RK3588|RK35*)
+    S922X*|RK3399|RK3588)
       add_emu_core gamecube dolphin dolphin-sa-gc true
       add_emu_core gamecube retroarch dolphin false
       add_es_system gamecube
@@ -470,7 +470,7 @@ makeinstall_target() {
       add_emu_core wii primehack primehack false
       add_es_system wii
     ;;
-    S922X*|RK3399|RK3588|RK35*)
+    S922X*|RK3399|RK3588)
       add_emu_core wii dolphin dolphin-sa-wii true
       add_emu_core wii retroarch dolphin false
       add_es_system wii
