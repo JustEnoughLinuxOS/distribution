c417648e5a956e7c6515c255134ae855b3df0cf1
byunjaeil <byunjaeil@gmail.com>
2023-10-17 07:01:28 +0900

    test

--- packages/virtual/es-themes/package.mk
+++ packages/virtual/es-themes/package.mk
@@ -4,6 +4,6 @@
 PKG_NAME="es-themes"
 PKG_LICENSE="Apache-2.0"
 PKG_SITE="www.jelos.org"
-PKG_DEPENDS_TARGET="es-theme-art-book-next"
+PKG_DEPENDS_TARGET="es-theme-art-book-dc es-theme-anbernic-dc"
 PKG_SECTION="virtual"
 PKG_LONGDESC="EmulationStation themes package."
