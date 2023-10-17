f07cd196283056caff1d65be5a066377715d2a94
fewtarius <fewtarius@jelos.org>
2023-10-17 13:11:57 +0000

    Re-enable spleen now that it has been patched to show the logo correctly.

--- packages/sysutils/spleen-font/package.mk
+++ packages/sysutils/spleen-font/package.mk
@@ -7,8 +7,8 @@ PKG_LICENSE="BSD-2-Clause"
 PKG_SITE="https://github.com/fcambus/spleen"
 PKG_URL="${PKG_SITE}/releases/download/${PKG_VERSION}/spleen-${PKG_VERSION}.tar.gz"
 
-PKG_DEPENDS_INIT="toolchain"
-PKG_DEPENDS_TARGET="toolchain"
+PKG_DEPENDS_INIT="toolchain bdf2psf:host"
+PKG_DEPENDS_TARGET="toolchain bdf2psf:host"
 PKG_LONGDESC="This package contains the Spleen Font"
 PKG_TOOLCHAIN="manual"
 
@@ -31,11 +31,18 @@ configure_target() {
 }
 
 make_init() {
-  :
+  for font in spleen-5x8 spleen-6x12
+  do
+    ${TOOLCHAIN}/usr/bin/bdf2psf --fb \
+            ${font}.bdf \
+            ${TOOLCHAIN}/usr/share/bdf2psf/standard.equivalents \
+            ${TOOLCHAIN}/usr/share/bdf2psf/ascii.set+${TOOLCHAIN}/usr/share/bdf2psf/linux.set+${TOOLCHAIN}/usr/share/bdf2psf/useful.set 512 \
+            ${font}.psfu
+  done
 }
 
 make_target() {
-  :
+  make_init
 }
 
 makeinstall_init() {
