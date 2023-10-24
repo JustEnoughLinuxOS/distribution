866b313ed29c22bcf65ec2316d9e47a930dc81cd
byunjaeil <byunjaeil@gmail.com>
2023-10-24 22:58:30 +0900

    test

--- packages/themes/es-theme-minielec/package.mk
+++ packages/themes/es-theme-minielec/package.mk
@@ -1,15 +1,15 @@
-# SPDX-License-Identifier: Apache-2.0
-# Copyright (C) 2022-present Fewtarius
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2023-present - The JELOS Project (https://github.com/JustEnoughLinuxOS)
 
-PKG_NAME="es-theme-minielec"
-PKG_VERSION="364173dab15a77190b11a5e36966890468cad67c"
+PKG_NAME="es-theme-minimal"
+PKG_VERSION="b478b0e3986e557b40fcd25a9e48d9956fa11a96"
 PKG_ARCH="any"
-PKG_LICENSE="CC BY-NC-SA 4.0"
-PKG_SITE="https://github.com/Rose22/es-theme-minielec"
+PKG_LICENSE="creative commons CC-BY-NC-SA"
+PKG_SITE="https://github.com/fabricecaruso/es-theme-minimal"
 PKG_URL="${PKG_SITE}.git"
 GET_HANDLER_SUPPORT="git"
-PKG_SHORTDESC="Minielec"
-PKG_LONGDESC="Minielec theme by Rose22"
+PKG_SHORTDESC="Minimal"
+PKG_LONGDESC="Minimal theme by fabricecaruso"
 PKG_TOOLCHAIN="manual"
 
 makeinstall_target() {
