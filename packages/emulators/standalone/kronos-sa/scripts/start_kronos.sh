7fad8282782cb97a6dff8fbe94f17db1937f8e52
fewtarius <fewtarius@jelos.org>
2023-10-26 21:10:35 +0000

    Finish basic kronos integration.

--- packages/emulators/standalone/kronos-sa/scripts/start_kronos.sh
+++ packages/emulators/standalone/kronos-sa/scripts/start_kronos.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # SPDX-License-Identifier: GPL-2.0-or-later
-# Copyright (C) 2021-present Shanti Gilbert (https://github.com/shantigilbert)
 # Copyright (C) 2023 JELOS (https://github.com/JustEnoughLinuxOS)
 
 # Source predefined functions and variables
@@ -10,7 +9,7 @@ jslisten set "-9 kronos"
 
 ROM_DIR="/storage/roms/saturn/kronos"
 CONFIG_DIR="/storage/.config/kronos/qt"
-SOURCE_DIR="/usr/config/kronos"
+SOURCE_DIR="/usr/config/kronos/qt"
 BIOS_BACKUP="/storage/roms/bios/kronos"
 
 if [ ! -d "${ROM_DIR}" ]
@@ -41,31 +40,80 @@ then
   USE_BIOS=$(get_setting use_hlebios saturn)
 fi
 
-if [ "$USE_BIOS" = 1 ]
+USE_SKIP=$(get_setting use_autoskip saturn "${GAME}")
+if [ "$USE_SKIP" = 1 ]
 then
-  for BIOS in saturn_bios.bin sega_101.bin mpr-17933.bin mpr-18811-mx.ic1 mpr-19367-mx.ic1 stvbios.zip
-  do
-    BIOS=$(find /storage/roms/bios -name ${BIOS} -print 2>/dev/null)
-    if [ ! -z "${BIOS}" ]
-    then
-      BIOS="-b ${BIOS}"
-      break
-    fi
-  done
+  AUTOSKIP="-autoframeskip 1"
 fi
 
-#Set the cores to use
-CORES=$(get_setting "cores" "${PLATFORM}" "${ROMNAME##*/}")
-if [ "${CORES}" = "little" ]
-then
-  EMUPERF="${SLOW_CORES}"
-elif [ "${CORES}" = "big" ]
-then
-  EMUPERF="${FAST_CORES}"
-else
-  ### All..
-  unset EMUPERF
-fi
+VIDEO_DRIVER=$(get_setting video_driver saturn "${GAME}")
+case ${VIDEO_DRIVER} in
+  opengl)
+    sed -i 's~Video\\VideoCore=.*$~Video\\VideoCore=1~g' ${CONFIG_DIR}/kronos.ini
+  ;;
+  *)
+    #Software
+    sed -i 's~Video\\VideoCore=.*$~Video\\VideoCore=2~g' ${CONFIG_DIR}/kronos.ini
+  ;;
+esac
 
-echo "Command: kronos -r 2 -i "${1}" ${BIOS}" >/var/log/exec.log 2>&1
-${EMUPERF} kronos -a -f  -i "${1}" -b ${BIOS} >>/var/log/exec.log 2>&1 ||:
+AUDIO_DRIVER=$(get_setting audio_driver saturn "${GAME}")
+case ${AUDIO_DRIVER} in
+  openal)
+    sed -i 's~Sound\\SoundCore=.*$~Sound\\SoundCore=4~g' ${CONFIG_DIR}/kronos.ini
+  ;;
+  *)
+    #SDL
+    sed -i 's~Sound\\SoundCore=.*$~Sound\\SoundCore=1~g' ${CONFIG_DIR}/kronos.ini
+  ;;
+esac
+
+SHOW_FPS=$(get_setting show_fps saturn "${GAME}")
+case ${SHOW_FPS} in
+  1)
+    sed -i 's~General\\ShowFPS=.*$~General\\ShowFPS=true~g' ${CONFIG_DIR}/kronos.ini
+  ;;
+  *)
+    sed -i 's~General\\ShowFPS=.*$~General\\ShowFPS=false~g' ${CONFIG_DIR}/kronos.ini
+  ;;
+esac
+
+USE_VSYNC=$(get_setting use_vsync saturn "${GAME}")
+case ${USE_VSYNC} in
+  1)
+    sed -i 's~General\\EnableVSync=.*$~General\\EnableVSync=true~g' ${CONFIG_DIR}/kronos.ini
+  ;;
+  *)
+    sed -i 's~General\\EnableVSync=.*$~General\\EnableVSync=false~g' ${CONFIG_DIR}/kronos.ini
+  ;;
+esac
+
+COMPUTE_SHADER=$(get_setting gpu_rgb saturn "${GAME}")
+Video\compute_shader_mode
+case ${COMPUTE_SHADER} in
+  1)
+    #gpu
+    sed -i 's~Video\\compute_shader_mode=.*$~Video\\compute_shader_mode=1~g' ${CONFIG_DIR}/kronos.ini
+  ;;
+  *)
+    #cpu
+    sed -i 's~Video\\compute_shader_mode=.*$~Video\\compute_shader_mode=1~g' ${CONFIG_DIR}/kronos.ini
+  ;;
+esac
+
+TESSELLATION=$(get_setting tessellation saturn "${GAME}")
+case ${TESSELLATION} in
+  gpu)
+    sed -i 's~Video\\polygon_generation_mode=.*$~Video\\polygon_generation_mode=2~g' ${CONFIG_DIR}/kronos.ini
+  ;;
+  *)
+    #cpu
+    sed -i 's~Video\\polygon_generation_mode=.*$~Video\\polygon_generation_mode=1~g' ${CONFIG_DIR}/kronos.ini
+  ;;
+esac
+
+#Get the number of active threads
+ACTIVE_THREADS=$(grep processor /proc/cpuinfo  | wc -l)
+sed -i 's~General\\NumThreads=.*$~General\\NumThreads='${ACTIVE_THREADS}'~g' ${CONFIG_DIR}/kronos.ini
+
+kronos -a -f  -i "${1}" ${BIOS} ${AUTOSKIP} >>/var/log/exec.log 2>&1 ||:
