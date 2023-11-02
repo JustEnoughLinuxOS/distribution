5f3d1ae477a84f67ac1b7e632267945638067ec3
fewtarius <fewtarius@jelos.org>
2023-11-02 11:59:23 +0000

    Enable debug tools by default across builds.

--- packages/virtual/image/package.mk
+++ packages/virtual/image/package.mk
@@ -55,14 +55,6 @@ fi
 # Add support for containers
 [ "${CONTAINER_SUPPORT}" = "yes" ] && PKG_DEPENDS_TARGET+=" ${PKG_TOOLS} docker"
 
-### Unless we're building the release branch, build the debug tools.
-BUILD_BRANCH="$(git branch --show-current)"
-if [ ! "${BUILD_BRANCH}" = "main" ] && \
-   [ -z "${DEBUG_SUPPORT}" ]
-then
-  DEBUG_SUPPORT=yes
-fi
-
 [ "${DEBUG_SUPPORT}" = "yes" ] && PKG_DEPENDS_TARGET+=" ${PKG_DEBUG}"
 
 # 32Bit package support
