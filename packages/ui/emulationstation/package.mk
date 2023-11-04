d1dedba69c6c85ff2715743dce866ab2a77de2ec
fewtarius <fewtarius@jelos.org>
2023-11-04 16:49:18 +0000

    Deprecate CW2015 charger on RK3566 builds, add standby as a suspend option.

--- packages/ui/emulationstation/package.mk
+++ packages/ui/emulationstation/package.mk
@@ -3,7 +3,7 @@
 # Copyright (C) 2023 JELOS (https://github.com/JustEnoughLinuxOS)
 
 PKG_NAME="emulationstation"
-PKG_VERSION="86f209752cc7eb47b22e3f00f807cf6d64ebc970"
+PKG_VERSION="33bf2d4"
 PKG_GIT_CLONE_BRANCH="main"
 PKG_REV="1"
 PKG_ARCH="any"
