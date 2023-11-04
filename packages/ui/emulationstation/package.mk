bf6e38474523e8f255312d5f0f60794fae6a9768
fewtarius <fewtarius@jelos.org>
2023-11-04 23:58:57 +0000

    Bump ES.

--- packages/ui/emulationstation/package.mk
+++ packages/ui/emulationstation/package.mk
@@ -3,7 +3,7 @@
 # Copyright (C) 2023 JELOS (https://github.com/JustEnoughLinuxOS)
 
 PKG_NAME="emulationstation"
-PKG_VERSION="2359410"
+PKG_VERSION="5155542"
 PKG_GIT_CLONE_BRANCH="main"
 PKG_REV="1"
 PKG_ARCH="any"
