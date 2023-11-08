5410f68551278861bbecf22841572ab5dedf361f
LittleBoi <byunjaeil@gmail.com>
2023-11-09 02:22:27 +0900

    Update package.mk

--- packages/themes/es-theme-art-book-dc/package.mk
+++ packages/themes/es-theme-art-book-dc/package.mk
@@ -4,7 +4,7 @@
 # Copyright (C) 2021 Fewtarius
 
 PKG_NAME="es-theme-art-book-dc"
-PKG_VERSION="2fb5a4f1eb54cc782a21e8cbd979e6fc235601d6"
+PKG_VERSION="970edf565ffafd00f11779e3eecaf8cb84c25055"
 PKG_ARCH="any"
 PKG_LICENSE="CUSTOM"
 PKG_SITE="https://github.com/UzuCore/es-theme-art-book-dc"
