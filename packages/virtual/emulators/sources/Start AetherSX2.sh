898b61bf6757e2218e1d85f78c0113a3455b76ca
mason <macebrooks@gmail.com>
2023-11-10 17:16:19 +0000

    Fix PS2 (Aethersx2) start script on Arm devices

---
+++ packages/virtual/emulators/sources/Start AetherSX2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2023 JELOS (https://github.com/JustEnoughLinuxOS)
+
+source /etc/profile
+
+jslisten set "aethersx2-sa"
+
+/usr/bin/aethersx2-sa >/dev/null 2>&1
