893a14a72a184abc32575d1a4acc3cf78de6ee85
fewtarius <fewtarius@jelos.org>
2023-12-03 16:22:09 +0000

    Additional merged storage optimizations, add a script to remove empty directories that may cause overlay issues.

---
+++ packages/misc/modules/sources/Optimize Merged Storage.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2023 JELOS (https://github.com/JustEnoughLinuxOS)
+
+source /etc/profile
+
+weston-terminal -f --command "/usr/bin/cleanup_overlay"
--- packages/ui/emulationstation/package.mk
+++ packages/ui/emulationstation/package.mk
@@ -3,7 +3,7 @@
 # Copyright (C) 2023 JELOS (https://github.com/JustEnoughLinuxOS)
 
 PKG_NAME="emulationstation"
-PKG_VERSION="a70b20b"
+PKG_VERSION="209ae15"
 PKG_GIT_CLONE_BRANCH="main"
 PKG_REV="1"
 PKG_ARCH="any"
