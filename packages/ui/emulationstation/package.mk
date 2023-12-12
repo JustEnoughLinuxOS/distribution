a8ccaac495871c5be4d850c0f8b6da7c3eba2d83
fewtarius <fewtarius@jelos.org>
2023-12-12 14:10:23 +0000

    Move creation of game directories out of automount and into ES under System Settings to remove merged storage directory conflict.

--- packages/ui/emulationstation/package.mk
+++ packages/ui/emulationstation/package.mk
@@ -3,7 +3,7 @@
 # Copyright (C) 2023 JELOS (https://github.com/JustEnoughLinuxOS)
 
 PKG_NAME="emulationstation"
-PKG_VERSION="db5ba85"
+PKG_VERSION="21d7c62"
 PKG_GIT_CLONE_BRANCH="main"
 PKG_REV="1"
 PKG_ARCH="any"
