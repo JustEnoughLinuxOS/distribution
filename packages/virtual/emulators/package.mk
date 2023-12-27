0d0ec8f3228b19fd63ca096c47016c8e8f449ffa
fewtarius <fewtarius@jelos.org>
2023-12-27 14:09:14 +0000

    pull over emulators virtual package from dev.

--- packages/virtual/emulators/package.mk
+++ packages/virtual/emulators/package.mk
@@ -16,8 +16,8 @@ PKG_RETROARCH="core-info libretro-database retroarch retroarch-assets retroarch-
 
 LIBRETRO_CORES="81-lr a5200-lr arduous-lr atari800-lr beetle-gba-lr beetle-lynx-lr beetle-ngp-lr beetle-pce-lr beetle-pce-fast-lr    \
                 beetle-pcfx-lr bsnes-lr bsnes-mercury-performance-lr beetle-supafaust-lr beetle-supergrafx-lr             \
-                beetle-vb-lr beetle-wswan-lr beetle-saturn-lr bluemsx-lr cannonball-lr cap32-lr crocods-lr daphne-lr      \
-                dinothawr-lr dosbox-pure-lr duckstation-lr easyrpg-lr fake08-lr fbalpha2012-lr                            \
+                beetle-vb-lr beetle-wswan-lr beetle-saturn-lr bluemsx-lr cap32-lr crocods-lr daphne-lr      \
+                dosbox-svn-lr dosbox-pure-lr duckstation-lr easyrpg-lr fake08-lr fbalpha2012-lr              \
                 fbalpha2019-lr fbneo-lr fceumm-lr flycast2021-lr fmsx-lr freechaf-lr freeintv-lr freej2me-lr fuse-lr      \
                 gambatte-lr gearboy-lr gearcoleco-lr gearsystem-lr genesis-plus-gx-lr genesis-plus-gx-wide-lr      \
                 gw-lr handy-lr hatari-lr idtech-lr mame2003-plus-lr mame2010-lr mame2015-lr melonds-lr      \
@@ -27,8 +27,6 @@ LIBRETRO_CORES="81-lr a5200-lr arduous-lr atari800-lr beetle-gba-lr beetle-lynx-
                 sameboy-lr sameduck-lr scummvm-lr smsplus-gx-lr snes9x-lr snes9x2002-lr snes9x2005_plus-lr snes9x2010-lr  \
                 stella-lr swanstation-lr tic80-lr tgbdual-lr uzem-lr vba-next-lr minivmac-lr               \
                 vbam-lr vecx-lr vice-lr yabasanshiro-lr virtualjaguar-lr xmil-lr"
-
-### Disabled 3566-ML: dosbox-svn-lr
 
 ### Emulators or cores for specific devices
 case "${DEVICE}" in
