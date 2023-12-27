c2d423f5368b9c467268d20ec9fbc4f644ef900b
fewtarius <fewtarius@jelos.org>
2023-12-27 12:07:37 +0000

    Fix building libvpx:host on RK3566.

--- packages/multimedia/libvpx/package.mk
+++ packages/multimedia/libvpx/package.mk
@@ -7,11 +7,13 @@ PKG_VERSION="df655cf4fb6c2a23b964544acd015cc715752830" # 1.13.1
 PKG_LICENSE="BSD"
 PKG_SITE="https://github.com/webmproject/libvpx"
 PKG_URL="${PKG_SITE}.git"
+PKG_DEPENDS_HOST="toolchain nasm:host"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="WebM VP8/VP9 Codec"
 
 configure_host() {
-  case ${ARCH} in
+  HOST_ARCH=$(uname -m)
+  case ${HOST_ARCH} in
     aarch64)
       PKG_HOST_NAME_LIBVPX="arm64-linux-gcc"
       ;;
@@ -22,7 +24,6 @@ configure_host() {
       PKG_HOST_NAME_LIBVPX="x86_64-linux-gcc"
       ;;
   esac
-
   ${PKG_CONFIGURE_SCRIPT} --prefix=${TOOLCHAIN} \
                         --extra-cflags="${CFLAGS}" \
                         --as=nasm \
