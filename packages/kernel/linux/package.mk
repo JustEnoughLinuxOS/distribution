9e214a369989ae08e8dd9dd91e39563fa36d7016
fewtarius <fewtarius@jelos.org>
2024-01-05 12:23:18 +0000

    Update kernel and patches for 6.7-rc8.

--- packages/kernel/linux/package.mk
+++ packages/kernel/linux/package.mk
@@ -4,8 +4,9 @@
 
 PKG_NAME="linux"
 PKG_LICENSE="GPL"
-PKG_VERSION="6.6.9"
-PKG_URL="https://www.kernel.org/pub/linux/kernel/v6.x/${PKG_NAME}-${PKG_VERSION}.tar.xz"
+PKG_VERSION="6.7-rc8"
+PKG_URL="https://git.kernel.org/torvalds/t/${PKG_NAME}-${PKG_VERSION}.tar.gz"
+#PKG_URL="https://www.kernel.org/pub/linux/kernel/v6.x/${PKG_NAME}-${PKG_VERSION}.tar.xz"
 PKG_SITE="http://www.kernel.org"
 PKG_DEPENDS_HOST="ccache:host rsync:host openssl:host rdfind:host"
 PKG_DEPENDS_TARGET="toolchain linux:host kmod:host cpio:host xz:host keyutils ncurses openssl:host wireless-regdb ${KERNEL_EXTRA_DEPENDS_TARGET}"
