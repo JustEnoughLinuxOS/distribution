2309746a2e5a602fac54c9de93ac07dbbc684150
fewtarius <fewtarius@jelos.org>
2024-01-19 10:48:17 +0000

    Revert openbor update.

--- packages/emulators/standalone/openbor/package.mk
+++ packages/emulators/standalone/openbor/package.mk
@@ -4,7 +4,7 @@
 # Copyright (C) 2023 JELOS (https://github.com/JustEnoughLinuxOS)
 
 PKG_NAME="openbor"
-PKG_VERSION="e838382583583f6f74115cf230df56170ca10f8b"
+PKG_VERSION="b8303cce992a0db93c3a465df3c943942fe322f8"
 PKG_ARCH="any"
 PKG_SITE="https://github.com/DCurrent/openbor"
 PKG_URL="${PKG_SITE}.git"
@@ -24,7 +24,6 @@ pre_configure_target() {
 
 pre_make_target() {
   cd ${PKG_BUILD}/engine
-  chmod 0755 version.sh
   ./version.sh
 }
 
