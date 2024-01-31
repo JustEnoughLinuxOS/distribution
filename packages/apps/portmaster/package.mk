ae0c08061934033e6b4824bbec26c2863b3fa96e
brooksytech <1673861+brooksytech@users.noreply.github.com>
2024-01-31 00:49:10 +0000

    Update Portmaster

--- packages/apps/portmaster/package.mk
+++ packages/apps/portmaster/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2022-present JELOS (https://github.com/JustEnoughLinuxOS)
 
 PKG_NAME="portmaster"
-PKG_VERSION="8.5.13_1130"
+PKG_VERSION="8.5.23_0623"
 PKG_SITE="https://github.com/PortsMaster/PortMaster-GUI"
 PKG_URL="${PKG_SITE}/releases/download/${PKG_VERSION}/PortMaster.zip"
 COMPAT_URL="https://github.com/brooksytech/JelosAddOns/raw/main/compat.zip"
