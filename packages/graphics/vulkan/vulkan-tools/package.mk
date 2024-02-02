4e17494f1f3924318afc4b257796dff2d6fff2ea
brooksytech <1673861+brooksytech@users.noreply.github.com>
2024-02-02 23:04:36 +0000

    Update vulkan to 1.3.277

--- packages/graphics/vulkan/vulkan-tools/package.mk
+++ packages/graphics/vulkan/vulkan-tools/package.mk
@@ -3,7 +3,7 @@
 # Copyright (C) 2021-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="vulkan-tools"
-PKG_VERSION="1.3.274"
+PKG_VERSION="1.3.277"
 PKG_LICENSE="Apache-2.0"
 PKG_SITE="https://github.com/KhronosGroup/Vulkan-Tools"
 PKG_URL="https://github.com/KhronosGroup/Vulkan-tools/archive/v${PKG_VERSION}.tar.gz"
