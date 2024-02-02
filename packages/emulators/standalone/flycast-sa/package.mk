48fac37670b3c63718f4456cbb3d5f2c42be1d10
brooksytech <1673861+brooksytech@users.noreply.github.com>
2024-02-02 23:02:11 +0000

    Update Yuzu-sa, Citra-SA, & Flycast-Sa

--- packages/emulators/standalone/flycast-sa/package.mk
+++ packages/emulators/standalone/flycast-sa/package.mk
@@ -3,7 +3,7 @@
 # Copyright (C) 2022-present JELOS (https://github.com/JustEnoughLinuxOS)
 
 PKG_NAME="flycast-sa"
-PKG_VERSION="6d6959880007344cfade656fdcffccde695b06e4"
+PKG_VERSION="57c88e07c41970975cb08425992abbeafe511ddd"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/flyinghead/flycast"
 PKG_URL="${PKG_SITE}.git"
