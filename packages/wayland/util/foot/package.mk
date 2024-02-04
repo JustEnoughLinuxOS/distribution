9ca00a002196f0812ff592abbb4e50216fc007de
sydarn <sydarn@proton.me>
2024-02-04 12:17:24 +0100

    Update some wayland related packages

--- packages/wayland/util/foot/package.mk
+++ packages/wayland/util/foot/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2021-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="foot"
-PKG_VERSION="1.14.0"
-PKG_SHA256="9a306951bc6bdce150364bccb0fb4b67720f50e98e9ac1de89792c1c1aa30690"
+PKG_VERSION="1.16.2"
+PKG_SHA256="8060ec28cbf6e2e3d408665330da4bc48fd094d4f1265d7c58dc75c767463c29"
 PKG_LICENSE="MIT"
 PKG_SITE="https://codeberg.org/dnkl/foot/"
 PKG_URL="https://codeberg.org/dnkl/foot/archive/${PKG_VERSION}.tar.gz"
