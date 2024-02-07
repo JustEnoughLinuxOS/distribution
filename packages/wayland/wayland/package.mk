f238f60782e1ee65d791383a9371294a0e6d2541
byunjaeil <byunjaeil@gmail.com>
2024-02-07 15:15:16 +0900

    Update package.mk

--- packages/wayland/wayland/package.mk
+++ packages/wayland/wayland/package.mk
@@ -8,16 +8,8 @@ PKG_DEPENDS_HOST="libffi:host expat:host libxml2:host"
 PKG_DEPENDS_TARGET="toolchain wayland:host libffi expat libxml2"
 PKG_LONGDESC="a display server protocol"
 
-case ${DEVICE} in
-  S922X)
-    PKG_VERSION="1.21.0"
-    PKG_SHA256="6dc64d7fc16837a693a51cfdb2e568db538bfdc9f457d4656285bb9594ef11ac"
-  ;;
-  *)
-    PKG_VERSION="1.22.0"
-    PKG_SHA256="1540af1ea698a471c2d8e9d288332c7e0fd360c8f1d12936ebb7e7cbc2425842"
-  ;;
-esac
+PKG_VERSION="1.22.0"
+PKG_SHA256="1540af1ea698a471c2d8e9d288332c7e0fd360c8f1d12936ebb7e7cbc2425842"
 
 PKG_URL="https://gitlab.freedesktop.org/wayland/wayland/-/releases/${PKG_VERSION}/downloads/${PKG_NAME}-${PKG_VERSION}.tar.xz"
 
