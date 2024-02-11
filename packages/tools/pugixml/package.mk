5227088890593dbcefb5fc8f1f29f0f3ab29b329
fewtarius <fewtarius@jelos.org>
2024-02-11 23:48:06 +0000

    Add support for displaying pico-8 carts as images, and update pugixml.

--- packages/tools/pugixml/package.mk
+++ packages/tools/pugixml/package.mk
@@ -2,10 +2,10 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="pugixml"
-PKG_VERSION="1.14"
+PKG_VERSION="b2b466403084667c90a0f0cc4e960405cfc8117a"
 PKG_LICENSE="MIT"
 PKG_SITE="https://pugixml.org/"
-PKG_URL="https://github.com/zeux/pugixml/archive/v${PKG_VERSION}.tar.gz"
+PKG_URL="https://github.com/zeux/pugixml.git"
 PKG_DEPENDS_HOST="toolchain:host"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="Light-weight, simple and fast XML parser for C++ with XPath support."
