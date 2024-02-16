9c7aaa96726ff745efd0d46c51efb4b267d1e966
fewtarius <fewtarius@jelos.org>
2024-02-16 02:11:33 +0000

    Bump ES, cleanup wxwidgets from previous changes.

--- packages/graphics/wxwidgets/package.mk
+++ packages/graphics/wxwidgets/package.mk
@@ -9,12 +9,6 @@ PKG_URL="${PKG_SITE}.git"
 PKG_GIT_CLONE_BRANCH="3.2"
 PKG_DEPENDS_TARGET="toolchain zlib libpng libjpeg-turbo gdk-pixbuf gtk3 libaio"
 PKG_LONGDESC="wxWidgets is a free and open source cross-platform C++ framework for writing advanced GUI applications using native controls."
-#PKG_BUILD_FLAGS="+pic"
-
-#pre_configure_target() {
-#  LDFLAGS="${LDFLAGS} -fPIC"
-#  CXXFLAGS="${CXXFLAGS} -fPIC"
-#}
 
 post_install() {
   cp ${PKG_BUILD}/.${TARGET_NAME}/lib/wx/config/gtk3-unicode-3.2 ${SYSROOT_PREFIX}/usr/bin/wx-config
