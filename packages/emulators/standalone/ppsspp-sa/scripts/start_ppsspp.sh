2ed01a6bf2de85d7e49cf606f8f74e51b7b0f63e
fewtarius <fewtarius@jelos.org>
2024-02-18 14:56:26 +0000

    Clean up PPSSPP launch, fix PPSSPP muting when the video backend changes.

--- packages/emulators/standalone/ppsspp-sa/scripts/start_ppsspp.sh
+++ packages/emulators/standalone/ppsspp-sa/scripts/start_ppsspp.sh
@@ -4,6 +4,15 @@
 
 . /etc/profile
 
+SOURCE_DIR="/usr/config/ppsspp"
+CONF_DIR="/storage/.config/ppsspp"
+PPSSPP_INI="/PSP/SYSTEM/ppsspp.ini"
+
+if [ ! -d "${CONF_DIR}" ]
+then
+  cp -rf ${SOURCE_DIR} ${CONF_DIR}
+fi
+
 #Set the cores to use
 CORES=$(get_setting "cores" "${PLATFORM}" "${ROMNAME##*/}")
 if [ "${CORES}" = "little" ]
@@ -27,98 +36,99 @@ fi
   VSYNC=$(get_setting vsync psp "${GAME}")
 
   #Frame Skip
-        if [ "$FSKIP" = "0" ]
+        if [ "${FSKIP}" = "0" ]
         then
-                sed -i '/^FrameSkip =/c\FrameSkip = 0' /storage/.config/ppsspp/PSP/SYSTEM/ppsspp.ini
-                sed -i '/^FrameSkipType =/c\FrameSkipType = 0' /storage/.config/ppsspp/PSP/SYSTEM/ppsspp.ini
-                sed -i '/^AutoFrameSkip =/c\AutoFrameSkip = False' /storage/.config/ppsspp/PSP/SYSTEM/ppsspp.ini
+                sed -i '/^FrameSkip =/c\FrameSkip = 0' ${CONF_DIR}/${PPSSPP_INI}
+                sed -i '/^FrameSkipType =/c\FrameSkipType = 0' ${CONF_DIR}/${PPSSPP_INI}
+                sed -i '/^AutoFrameSkip =/c\AutoFrameSkip = False' ${CONF_DIR}/${PPSSPP_INI}
         fi
-        if [ "$FSKIP" = "1" ]
+        if [ "${FSKIP}" = "1" ]
         then
-                sed -i '/^FrameSkip =/c\FrameSkip = 1' /storage/.config/ppsspp/PSP/SYSTEM/ppsspp.ini
-                sed -i '/^FrameSkipType =/c\FrameSkipType = 0' /storage/.config/ppsspp/PSP/SYSTEM/ppsspp.ini
-                sed -i '/^AutoFrameSkip =/c\AutoFrameSkip = False' /storage/.config/ppsspp/PSP/SYSTEM/ppsspp.ini
+                sed -i '/^FrameSkip =/c\FrameSkip = 1' ${CONF_DIR}/${PPSSPP_INI}
+                sed -i '/^FrameSkipType =/c\FrameSkipType = 0' ${CONF_DIR}/${PPSSPP_INI}
+                sed -i '/^AutoFrameSkip =/c\AutoFrameSkip = False' ${CONF_DIR}/${PPSSPP_INI}
         fi
-        if [ "$FSKIP" = "2" ]
+        if [ "${FSKIP}" = "2" ]
         then
-                sed -i '/^FrameSkip =/c\FrameSkip = 2' /storage/.config/ppsspp/PSP/SYSTEM/ppsspp.ini
-                sed -i '/^FrameSkipType =/c\FrameSkipType = 0' /storage/.config/ppsspp/PSP/SYSTEM/ppsspp.ini
-                sed -i '/^AutoFrameSkip =/c\AutoFrameSkip = False' /storage/.config/ppsspp/PSP/SYSTEM/ppsspp.ini
+                sed -i '/^FrameSkip =/c\FrameSkip = 2' ${CONF_DIR}/${PPSSPP_INI}
+                sed -i '/^FrameSkipType =/c\FrameSkipType = 0' ${CONF_DIR}/${PPSSPP_INI}
+                sed -i '/^AutoFrameSkip =/c\AutoFrameSkip = False' ${CONF_DIR}/${PPSSPP_INI}
         fi
-        if [ "$FSKIP" = "3" ]
+        if [ "${FSKIP}" = "3" ]
         then
-                sed -i '/^FrameSkip =/c\FrameSkip = 3' /storage/.config/ppsspp/PSP/SYSTEM/ppsspp.ini
-                sed -i '/^FrameSkipType =/c\FrameSkipType = 0' /storage/.config/ppsspp/PSP/SYSTEM/ppsspp.ini
-                sed -i '/^AutoFrameSkip =/c\AutoFrameSkip = False' /storage/.config/ppsspp/PSP/SYSTEM/ppsspp.ini
+                sed -i '/^FrameSkip =/c\FrameSkip = 3' ${CONF_DIR}/${PPSSPP_INI}
+                sed -i '/^FrameSkipType =/c\FrameSkipType = 0' ${CONF_DIR}/${PPSSPP_INI}
+                sed -i '/^AutoFrameSkip =/c\AutoFrameSkip = False' ${CONF_DIR}/${PPSSPP_INI}
         fi
-        if [ "$FSKIP" = "auto" ]
+        if [ "${FSKIP}" = "auto" ]
         then
-                sed -i '/AutoFrameSkip =/c\AutoFrameSkip = True' /storage/.config/ppsspp/PSP/SYSTEM/ppsspp.ini
+                sed -i '/AutoFrameSkip =/c\AutoFrameSkip = True' ${CONF_DIR}/${PPSSPP_INI}
         fi
 
 
   #Graphics Backend
         #Default to OpenGL / GLES if no option is set.
-        sed -i '/^GraphicsBackend =/c\GraphicsBackend = 0 (OPENGL)' /storage/.config/ppsspp/PSP/SYSTEM/ppsspp.ini
+        sed -i '/^GraphicsBackend =/c\GraphicsBackend = 0 (OPENGL)' ${CONF_DIR}/${PPSSPP_INI}
 
-        if [ "$GRENDERER" = "opengl" ]
+        if [ "${GRENDERER}" = "opengl" ]
         then
-                sed -i '/^GraphicsBackend =/c\GraphicsBackend = 0 (OPENGL)' /storage/.config/ppsspp/PSP/SYSTEM/ppsspp.ini
+                sed -i '/^GraphicsBackend =/c\GraphicsBackend = 0 (OPENGL)' ${CONF_DIR}/${PPSSPP_INI}
         fi
-        if [ "$GRENDERER" = "vulkan" ]
+        if [ "${GRENDERER}" = "vulkan" ]
         then
-                sed -i '/^GraphicsBackend =/c\GraphicsBackend = 3 (VULKAN)' /storage/.config/ppsspp/PSP/SYSTEM/ppsspp.ini
+                sed -i '/^GraphicsBackend =/c\GraphicsBackend = 3 (VULKAN)' ${CONF_DIR}/${PPSSPP_INI}
         fi
 
   #Internal Resolution
-        if [ "$IRES" = "1" ]
+        if [ "${IRES}" = "1" ]
         then
-                sed -i '/^InternalResolution/c\InternalResolution = 1' /storage/.config/ppsspp/PSP/SYSTEM/ppsspp.ini
+                sed -i '/^InternalResolution/c\InternalResolution = 1' ${CONF_DIR}/${PPSSPP_INI}
         fi
-        if [ "$IRES" = "2" ]
+        if [ "${IRES}" = "2" ]
         then
-                sed -i '/^InternalResolution/c\InternalResolution = 2' /storage/.config/ppsspp/PSP/SYSTEM/ppsspp.ini
+                sed -i '/^InternalResolution/c\InternalResolution = 2' ${CONF_DIR}/${PPSSPP_INI}
         fi
-        if [ "$IRES" = "3" ]
+        if [ "${IRES}" = "3" ]
         then
-                sed -i '/^InternalResolution/c\InternalResolution = 3' /storage/.config/ppsspp/PSP/SYSTEM/ppsspp.ini
+                sed -i '/^InternalResolution/c\InternalResolution = 3' ${CONF_DIR}/${PPSSPP_INI}
         fi
-        if [ "$IRES" = "4" ]
+        if [ "${IRES}" = "4" ]
         then
-                sed -i '/^InternalResolution/c\InternalResolution = 4' /storage/.config/ppsspp/PSP/SYSTEM/ppsspp.ini
+                sed -i '/^InternalResolution/c\InternalResolution = 4' ${CONF_DIR}/${PPSSPP_INI}
         fi
 
   #Show FPS
-	if [ "$FPS" = "0" ]
+	if [ "${FPS}" = "0" ]
 	then
-  		sed -i '/^iShowStatusFlags =/c\iShowStatusFlags = 0' /storage/.config/ppsspp/PSP/SYSTEM/ppsspp.ini
+  		sed -i '/^iShowStatusFlags =/c\iShowStatusFlags = 0' ${CONF_DIR}/${PPSSPP_INI}
 	fi
-	if [ "$FPS" = "1" ]
+	if [ "${FPS}" = "1" ]
 	then
-  		sed -i '/^iShowStatusFlags =/c\iShowStatusFlags = 2' /storage/.config/ppsspp/PSP/SYSTEM/ppsspp.ini
+  		sed -i '/^iShowStatusFlags =/c\iShowStatusFlags = 2' ${CONF_DIR}/${PPSSPP_INI}
 	fi
 
   #Skip Buffer Effects
-        if [ "$SKIPB" = "0" ]
+        if [ "${SKIPB}" = "0" ]
         then
-                sed -i '/^SkipBufferEffects =/c\SkipBufferEffects = False' /storage/.config/ppsspp/PSP/SYSTEM/ppsspp.ini
+                sed -i '/^SkipBufferEffects =/c\SkipBufferEffects = False' ${CONF_DIR}/${PPSSPP_INI}
         fi
-        if [ "$SKIPB" = "1" ]
+        if [ "${SKIPB}" = "1" ]
         then
-                sed -i '/^SkipBufferEffects =/c\SkipBufferEffects = True' /storage/.config/ppsspp/PSP/SYSTEM/ppsspp.ini
+                sed -i '/^SkipBufferEffects =/c\SkipBufferEffects = True' ${CONF_DIR}/${PPSSPP_INI}
         fi
 
   #VSYNC
-        if [ "$VSYNC" = "0" ]
+        if [ "${VSYNC}" = "0" ]
         then
-                sed -i '/^VSyncInterval =/c\VSyncInterval = False' /storage/.config/ppsspp/PSP/SYSTEM/ppsspp.ini
+                sed -i '/^VSyncInterval =/c\VSyncInterval = False' ${CONF_DIR}/${PPSSPP_INI}
         fi
-        if [ "$VSYNC" = "1" ]
+        if [ "${VSYNC}" = "1" ]
         then
-                sed -i '/^VSyncInterval =/c\VSyncInterval = True' /storage/.config/ppsspp/PSP/SYSTEM/ppsspp.ini
+                sed -i '/^VSyncInterval =/c\VSyncInterval = True' ${CONF_DIR}/${PPSSPP_INI}
         fi
 
 ARG=${1//[\\]/}
+
 jslisten set "-9 ppsspp"
-export SDL_AUDIODRIVER=alsa
+
 ${EMUPERF} ppsspp --pause-menu-exit "${ARG}"
